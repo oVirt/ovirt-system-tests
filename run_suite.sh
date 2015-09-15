42ab23d79ecb5174cd3d960e0914ea2d16202bb7
David Caro <dcaroest@redhat.com>
2015-09-15 18:28:28 +0200

    Some bash improvements
    * Adapted to the ci standards
    * Now the options are properly parsed and the -h works
    
    Change-Id: Ia3e5286d0fc6c2f340b2f908f79b55ed636073b0
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- run_suite.sh
+++ run_suite.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -e
-
 CLI="lagocli"
 
-if [[ ! -z "$ENGINE_BUILD_GWT" ]];
-then
-	ENGINE_WITH_GWT="--engine-with-gwt"
+if [[ -n "$ENGINE_BUILD_GWT" ]]; then
+    ENGINE_WITH_GWT="--engine-with-gwt"
 fi
 
+
 usage () {
-	echo "
+    echo "
 Usage:
 
 $0 SUITE [-o|--output path] [-e|--engine path] [-v|--vdsm path] [-i|--ioprocess path]
@@ -16,112 +15,121 @@ $0 SUITE [-o|--output path] [-e|--engine path] [-v|--vdsm path] [-i|--ioprocess 
 This script runs a single suite of tests (a directory of tests repo)
 
 Positional arguments:
-	SUITE
-		Path to directory that contains the suite to be executed
+    SUITE
+        Path to directory that contains the suite to be executed
 
 Optional arguments:
-	-o,--output PATH
-		Path where the new environment will be deployed.
+    -o,--output PATH
+        Path where the new environment will be deployed.
 
-	-e,--engine PATH
-		Path to ovirt-engine source that will be available in the environment
+    -e,--engine PATH
+        Path to ovirt-engine source that will be available in the environment
 
-	-v,--vdsm PATH
-		Path to vdsm source that will be available in the environment
+    -v,--vdsm PATH
+        Path to vdsm source that will be available in the environment
 
-	-i,--ioprocess PATH
-		Path to ioprocess source that will be available in the environment
+    -i,--ioprocess PATH
+        Path to ioprocess source that will be available in the environment
 "
 }
 
+
 env_init () {
-	$CLI init 		\
-		$PREFIX			\
-		$SUITE/init.json	\
-		--template-repo-path $SUITE/template-repo.json
+    $CLI init \
+        $PREFIX \
+        $SUITE/init.json \
+        --template-repo-path $SUITE/template-repo.json
 }
+
 
 env_repo_setup () {
-	cd $PREFIX
-	$CLI ovirt reposetup 	\
-		--reposync-yum-config $SUITE/reposync-config.repo \
-		--engine-dir=$ENGINE_DIR \
-		$ENGINE_WITH_GWT \
-		--vdsm-dir=$VDSM_DIR \
-		--ioprocess-dir=$IOPROCESS_DIR
+    cd $PREFIX
+    $CLI ovirt reposetup \
+        --reposync-yum-config $SUITE/reposync-config.repo \
+        --engine-dir=$ENGINE_DIR \
+        $ENGINE_WITH_GWT \
+        --vdsm-dir=$VDSM_DIR \
+        --ioprocess-dir=$IOPROCESS_DIR
 }
+
 
 env_start () {
-	cd $PREFIX
-	$CLI start
+    cd $PREFIX
+    $CLI start
 }
+
 
 env_deploy () {
-	cd $PREFIX
-	$CLI ovirt deploy
+    cd $PREFIX
+    $CLI ovirt deploy
 }
+
 
 env_run_test () {
-	cd $PREFIX
-	$CLI ovirt runtest $1
+    cd $PREFIX
+    $CLI ovirt runtest $1
 }
+
 
 env_collect () {
-	cd $PREFIX
-	$CLI ovirt collect --output $1
+    cd $PREFIX
+    $CLI ovirt collect --output $1
 }
 
-if [ $# -lt 1 ];
-then
-	usage
-	exit 1
+options=$( \
+    getopt \
+        -o ho:v:e:i: \
+        --long help,output:,vdsm:,engine:,ioprocess: \
+        -n 'run_suite.sh' \
+        -- "$@" \
+)
+if [[ "$?" != "0" ]]; then
+    exit 1
+fi
+eval set -- "$options"
+
+while true; do
+    case $1 in
+        -o|--output)
+            PREFIX=$(realpath $2)
+            shift 2
+            ;;
+        -v|--vdsm)
+            VDSM_DIR=$(realpath $2)
+            shift 2
+            ;;
+        -e|--engine)
+            ENGINE_DIR=$(realpath $2)
+            shift 2
+            ;;
+        -i|--ioprocess)
+            IOPROCESS_DIR=$(realpath $2)
+            shift 2
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+if [[ -z "$1" ]]; then
+    echo "ERROR: no suite passed"
+    usage
+    exit 1
 fi
 
-export SUITE=$(realpath $1);
-export PREFIX=$PWD/deployment-$(basename $SUITE)
-shift 1;
-
-TEMP=$(getopt -o ho:v:e:i: --long help,output:,vdsm:,engine:,ioprocess: -n 'run_suite.sh' -- "$@")
-eval set -- "$TEMP"
-
-while true ; do
-	case $1 in
-		-o|--output)
-			PREFIX=$(realpath $2)
-			shift 2
-			;;
-		-v|--vdsm)
-			VDSM_DIR=$(realpath $2)
-			shift 2
-			;;
-		-e|--engine)
-			ENGINE_DIR=$(realpath $2)
-			shift 2
-			;;
-		-i|--ioprocess)
-			IOPROCESS_DIR=$(realpath $2)
-			shift 2
-			;;
-		-h|--help)
-			usage
-			exit 0
-			;;
-		--)
-			shift
-			break
-			;;
-		*)
-			echo "Invalid argument $1"
-			usage
-			exit 1
-			;;
-	esac
-done
+export SUITE="$(realpath "$1")"
+export PREFIX="$PWD/deployment-${SUITE##*/}"
 
 echo "Running suite found in ${SUITE}"
 echo "Environment will be deployed at ${PREFIX}"
 
-. ${SUITE}/control.sh
+source "${SUITE}/control.sh"
 
 prep_suite
 run_suite

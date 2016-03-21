b05f6a4fe2473cfd682e6f65075b2a6c119f323b
Yaniv Kaul <ykaul@redhat.com>
2016-03-21 22:53:26 +0200

    Workaround for https://bugzilla.redhat.com/show_bug.cgi?id=1258868
    It delays tuned initialization, as dbus is rejecting 'partial' files
    that were just installed via RPM. Workaround: restart dbus.
    
    Cuts 2 minutes from host installation.
    
    Change-Id: I92abe8c671ef1d4aa58ea82b130ccbc3111230af
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>

--- common/deploy-scripts/setup_host.sh
+++ common/deploy-scripts/setup_host.sh
@@ -4,6 +4,11 @@ cp /etc/sysconfig/network-scripts/ifcfg-eth0 /tmp/tmp
 cat /tmp/tmp | grep -v HWADDR > /etc/sysconfig/network-scripts/ifcfg-eth0
 rm -f /tmp/tmp
 
+#workaround for https://bugzilla.redhat.com/show_bug.cgi?id=1258868
+# It delays tuned initialization, as dbus is rejecting 'partial' files
+# that were just installed via RPM. Workaround: restart dbus.
+# Cuts 2 minutes from host installation
+yum update -y tuned && systemctl restart dbus
 
 yum install --nogpgcheck -y vdsm
 

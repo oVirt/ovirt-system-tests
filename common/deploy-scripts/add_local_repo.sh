a2229b6860c965fb7359f8bd848d58b9ff325313
David Caro <dcaroest@redhat.com>
2016-04-28 14:12:21 +0200

    master:Use only the local repo
    Change-Id: I8b8442d680a1c29f57965527a830290f33f4f6e8

--- common/deploy-scripts/add_local_repo.sh
+++ common/deploy-scripts/add_local_repo.sh
@@ -1,3 +1,4 @@
+set -xe
 DIST=$(uname -r | sed -r  's/^.*\.([^\.]+)\.[^\.]+$/\1/')
 ADDR=$(ip -4 addr show scope global up |grep -m1 inet | awk '{split($4,a,"."); print a[1] "." a[2] "." a[3] ".1"}')
 
@@ -10,3 +11,9 @@ skip_if_unavailable=1
 gpgcheck=0
 cost=1
 EOF
+
+# disable any other repos to avoid downloading metadata
+yum install --disablerepo=\* --enablerepo=alocalsync -y yum-utils
+yum-config-manager --disable \*
+yum-config-manager --enable alocalsync
+

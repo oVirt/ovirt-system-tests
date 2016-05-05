8527c80fedba11ae94ab3136bc1e842f61ffbcdd
Tolik Litovsky <tlitovsk@redhat.com>
2016-05-05 07:33:38 +0300

    Avoiding the install of deltarpm in the engine appliance
    Change-Id: I7587ee7b6346c5d1e59a689388036d17bac28b5d
    Signed-off-by: Tolik Litovsky <tlitovsk@redhat.com>

---
+++ image_ng_suite_3.6/deploy-scripts/setup_engine.sh
@@ -0,0 +1,18 @@
+set -xe
+
+cat > /root/iso-uploader.conf << EOF
+[ISOUploader]
+user=admin@internal
+passwd=123
+engine=localhost:443
+EOF
+
+# engine 4 resolves its FQDN
+ADDR=$(/sbin/ip -4 -o addr show dev eth0 | awk '{split($4,a,"."); print a[1] "." a[2] "." a[3] "." a[4]}'| awk -F/ '{print $1}')
+echo "$ADDR engine" >> /etc/hosts
+
+# Enable debug logs on the engine
+sed -i \
+    -e '/.*logger category="org.ovirt"/{ n; s/INFO/DEBUG/ }' \
+    -e '/.*<root-logger>/{ n; s/INFO/DEBUG/ }' \
+    /usr/share/ovirt-engine/services/ovirt-engine/ovirt-engine.xml.in

158d3c6235763d190b25ae2157685725d16c06f1
Sahina Bose <sabose@redhat.com>
2016-05-09 23:47:48 +0300

    ovirt-system-tests: basic Gluster Hyper-converged setup
    This sets up an HC setup with 3 hosts. One of the hosts is used
    as the controller node to run gdeploy.
    Storage is served using gluster volume created
    across the 3 hosts
    
    Added also add automation links so we can run it in CI.
    
    Change-Id: I9be79b97f3c4423cfb9fe98fba4ce28b712607df
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>
    Signed-off-by: Sahina Bose <sabose@redhat.com>

---
+++ hc-basic-suite-master/generate-hc-answerfile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+HOST1IP=$1
+HOST2IP=$2
+HOST3IP=$3
+HOSTEDENGINE="hc-engine.lago.local"
+
+MYADDR=$(\
+    /sbin/ip -4 -o addr show dev eth0 \
+    | awk '{split($4,a,"."); print a[1] "." a[2] "." a[3] "." a[4]}'\
+    | awk -F/ '{print $1}'\
+)
+MYHOSTNAME=$(hostname | sed s/_/-/g).lago.local
+
+echo "${MYADDR} ${MYHOSTNAME}$DOMAIN" >> /etc/hosts
+
+HEGW=$(\
+    /sbin/ip -4 -o addr show dev eth0 \
+    | awk '{split($4,a,"."); print a[1] "." a[2] "." a[3] ".1"}'\
+    | awk -F/ '{print $1}'\
+)
+HEADDR=$(\
+    /sbin/ip -4 -o addr show dev eth0 \
+    | awk '{split($4,a,"."); print a[1] "." a[2] "." a[3] ".99"}'\
+    | awk -F/ '{print $1}'\
+)
+echo "${HEADDR} ${HOSTEDENGINE}" >> /etc/hosts
+
+OVAIMAGE=$(\
+    ls /usr/share/ovirt-engine-appliance/ovirt-engine-appliance-*.ova \
+    | tail -11\
+)
+VMPASS=123456
+ENGINEPASS=123
+
+sed \
+    -e "s,@GW@,${HEGW},g" \
+    -e "s,@ADDR@,${HEADDR},g" \
+    -e "s,@OVAIMAGE@,${OVAIMAGE},g" \
+    -e "s,@VMPASS@,${VMPASS},g" \
+    -e "s,@ENGINEPASS@,${ENGINEPASS},g" \
+    -e "s,@HOSTEDENGINE@,${HOSTEDENGINE},g" \
+    -e "s,@HOST1-IP@,${HOST1IP},g" \
+    -e "s,@HOST2-IP@,${HOST2IP},g" \
+    -e "s,@HOST3-IP@,${HOST3IP},g" \
+    < /root/hc-answers.conf.in \
+    > /root/hc-answers.conf

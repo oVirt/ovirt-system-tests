9fcd2001ffb4cf41a3b31f4fea3431dc07b05227
Nadav Goldin <ngoldin@redhat.com>
2016-11-07 11:41:21 +0200

    Add fall back to 'extra_sources' file in suite.sh
    1. Changed back filename lookup: 'extra-sources' to 'extra_sources'
    (partly reverts d0e06f932880f7149a022755cc942ed9493346bd),
    because the jenkins groovy codes looks for 'extra_sources'. The
    previous patch had hostname naming conventions in mind(do not
    allow '_' in hostnames), but that has no strict relation to
    'extra_sources' filename.
    
    2. Changed the order of 'extra_sources' lookup:
    $SUITE/extra_sources, if it does not exist:
    $PWD/extra_sources, if does not exists: ignore.
    
    3. Fixed minor style issues(missing '"')
    
    Change-Id: Id91369f4b88c9334988007806d0b449a545fe5a5
    Signed-off-by: Nadav Goldin <ngoldin@redhat.com>

--- automation/suite.sh
+++ automation/suite.sh
@@ -17,18 +17,18 @@ SUITE=${SUITE##*/}
 SUITE=${SUITE%.*}
 echo "Running suite: $SUITE"
 
-SUITE_REAL_PATH=$(realpath $SUITE)
+SUITE_REAL_PATH=$(realpath "$SUITE")
 
 cleanup() {
     rm -rf exported-artifacts
     mkdir -p exported-artifacts
     [[ -d deployment-$SUITE/current/logs ]] \
-    && mv deployment-$SUITE/current/logs exported-artifacts/lago_logs
-    find deployment-$SUITE \
+    && mv "deployment-$SUITE/current/logs" exported-artifacts/lago_logs
+    find "deployment-$SUITE" \
         -iname nose\*.xml \
         -exec mv {} exported-artifacts/ \;
     [[ -d test_logs ]] && mv test_logs exported-artifacts/
-    ./run_suite.sh --cleanup $SUITE
+    ./run_suite.sh --cleanup "$SUITE"
     exit
 }
 
@@ -40,12 +40,26 @@ trap cleanup SIGTERM EXIT
 res=0
 
 # This is used to test external sources
-# it's done by putting them one per line in $SUITE/extra-sources file
-extra_sources_cmd=''
-if [[ -e $SUITE/extra-sources ]]; then
-    extra_sources_cmd+="-s \"conf:$SUITE_REAL_PATH/extra-sources\""
+# it's done by putting them one per line in $SUITE/extra-sources file, the
+# It will look for:
+# 1. $SUITE/extra_sources
+# 2. $PWD/extra_sources
+# Example:
+# > cat extra_sources
+# http://plain.resources.ovirt.org/repos/ovirt/experimental/master/latest.under_testing/
+#
+extra_sources_cmd=""
+if [[ -e "$SUITE_REAL_PATH/extra_sources" ]]; then
+    cat "$SUITE_REAL_PATH/extra_sources"
+    extra_sources_cmd="-s \"conf:$SUITE_REAL_PATH/extra_sources\""
+elif [[ -e "$PWD/extra_sources" ]]; then
+    cat "$PWD/extra_sources"
+    extra_sources_cmd="-s \"conf:$PWD/extra_sources\""
 fi
 
-./run_suite.sh $extra_sources_cmd $SUITE \
-|| res=$?
+if [[ -z "$extra_sources_cmd" ]]; then
+    ./run_suite.sh "$SUITE" || res=$?
+else
+    ./run_suite.sh "$extra_sources_cmd" "$SUITE" || res=$?
+fi
 exit $res

3061b7afdcf4f94f7a617dbad713bc8b95dc16ea
Nadav Goldin <ngoldin@redhat.com>
2016-12-13 12:26:40 +0200

    Ensure /dev/kvm exists in mock chroot
    If /dev/kvm doesn't exist inside mock's chroot, libguestfs
    will still use 'direct' backend but without KVM, which is
    much slower.
    
    Change-Id: Ie89565cd6f200fa7bfe6d6aa80192d7dc192fd0f
    Signed-off-by: Nadav Goldin <ngoldin@redhat.com>

--- automation/suite.sh
+++ automation/suite.sh
@@ -34,7 +34,11 @@ cleanup() {
 }
 
 # needed to run lago inside chroot
+# TO-DO: use libvirt backend instead
 export LIBGUESTFS_BACKEND=direct
+# ensure /dev/kvm exists, otherwise it will still use
+# direct backend, but without KVM(much slower).
+! [[ -c "/dev/kvm" ]] && mknod /dev/kvm c 10 232
 # uncomment the next lines for extra verbose output
 #export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
 trap cleanup SIGTERM EXIT

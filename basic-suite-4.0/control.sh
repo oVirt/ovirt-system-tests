66f8d35adc33d3e26d3cc3e55e262f50dac755cc
Eyal Edri <eedri@redhat.com>
2017-03-22 17:05:24 +0200

    removing 4.0 suites
    also removed automation scripts
    
    Change-Id: If4e7d4af56914eee5a7fcb1746a86894c4313bff
    Signed-off-by: Eyal Edri <eedri@redhat.com>

--- basic-suite-4.0/control.sh
+++
@@ -1,1 +0,0 @@
-../basic-suite-3.6/control.sh
--- he-basic-suite-3.6/control.sh
+++ he-basic-suite-3.6/control.sh
@@ -1,1 +1,97 @@
-../he-basic-suite-4.0/control.sh
+#!/bin/bash -xe
+set -o pipefail
+
+
+prep_suite () {
+    local suite_name="${SUITE##*/}"
+    suite_name="${suite_name//./-}"
+    sed -r \
+        -e "s,__ENGINE__,lago-${suite_name}-engine,g" \
+        -e "s,__HOST([0-9]+)__,lago-${suite_name}-host\1,g" \
+        -e "s,__LAGO_NET__,lago-${suite_name}-lago,g" \
+        -e "s,__STORAGE__,lago-${suite_name}-storage,g" \
+    < ${SUITE}/LagoInitFile.in \
+    > ${SUITE}/LagoInitFile
+}
+
+
+he_deploy() {
+    local suite="${SUITE?}"
+    local curdir="${PWD?}"
+    local suite_name="${SUITE##*/}"
+    suite_name="${suite_name//./-}"
+    HOST=lago-${suite_name}-host
+    cd $PREFIX
+    echo "#########################"
+    echo "Deploying on ${HOST}0"
+    lago copy-to-vm \
+        ${HOST}0 \
+        "${SUITE}/answers.conf.in" \
+        /root/hosted-engine-deploy-answers-file.conf.in
+
+    lago copy-to-vm \
+        ${HOST}0 \
+        "${SUITE}/setup_first_he_host.sh" \
+        /root/
+
+    lago shell \
+        ${HOST}0 \
+        /root/setup_first_he_host.sh
+    RET_CODE=$?
+    if [ ${RET_CODE} -ne 0 ]; then
+        echo "hosted-engine setup on ${HOST}0 failed with status ${RET_CODE}."
+        exit ${RET_CODE}
+    fi
+
+    echo "#########################"
+    echo "Deploying on ${HOST}1"
+    lago copy-to-vm \
+        ${HOST}1 \
+        "${SUITE}/answers-additional.conf.in" \
+        /root/hosted-engine-deploy-answers-file.conf.in
+
+    lago copy-to-vm \
+        ${HOST}1 \
+        "${SUITE}/setup_additional_he_host.sh" \
+        /root/
+
+    lago shell \
+        ${HOST}1 \
+        /root/setup_additional_he_host.sh
+    RET_CODE=$?
+    if [ ${RET_CODE} -ne 0 ]; then
+        echo "hosted-engine setup on ${HOST}1 failed with status ${RET_CODE}."
+        exit ${RET_CODE}
+    fi
+    cd -
+}
+
+run_suite(){
+    local suite="${SUITE?}"
+    local curdir="${PWD?}"
+    declare failed=false
+    env_init \
+        "http://templates.ovirt.org/repo/repo.metadata" \
+        "$suite/LagoInitFile"
+    env_repo_setup
+    env_start
+    env_deploy
+    he_deploy || failed=true
+    if $failed; then
+        env_collect "$curdir/test_logs/${suite##*/}/post-he_deploy"
+        echo "@@@@ ERROR: Failed running he_deploy"
+        return 1
+    fi
+
+    declare test_scenarios=($(ls "$suite"/test-scenarios/*.py | sort))
+
+    for scenario in "${test_scenarios[@]}"; do
+        echo "Running test scenario ${scenario##*/}"
+        env_run_test "$scenario" || failed=true
+        env_collect "$curdir/test_logs/${suite##*/}/post-${scenario##*/}"
+        if $failed; then
+            echo "@@@@ ERROR: Failed running $scenario"
+            break
+        fi
+    done
+}

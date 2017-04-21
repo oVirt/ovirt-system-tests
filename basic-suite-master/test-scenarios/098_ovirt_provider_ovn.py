a9e77133ca8c48f821d45d4eb8829bbec78979a4
Marcin Mirecki <mmirecki@redhat.com>
2017-04-21 13:33:07 +0200

    Adding scenario testing ovirt-provider-ovn
    This patch checks if ovirt-provider-ovn
    responds to request after being installed
    by engine setup.
    The tests makes an authorization request, checking
    if the recieved token is of correct format, and
    a basic get networks request (receiving an empty
    list of networks).
    Note, that this patch does not check the correctness
    of the certificates used for the SSL connection.
    This will hopefully come in later patches.
    
    Change-Id: I0ee1ff01da43ed72544d766aa89e07dc5fcf8ace
    Signed-off-by: Marcin Mirecki<mmirecki@redhat.com>

---
+++ basic-suite-master/test-scenarios/098_ovirt_provider_ovn.py
@@ -0,0 +1,113 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import json
+import requests
+import uuid
+
+import nose.tools as nt
+
+from ovirtlago import testlib
+
+
+OVN_PROVIDER_TOKEN_URL = 'https://{hostname}:35357/v2.0/tokens'
+OVN_PROVIDER_NETWORKS_URL = 'https://{hostname}:9696/v2.0/networks'
+
+
+def _request_auth_token(engine_name):
+    auth_request_data = {
+        "auth": {
+            "tenantName": "ovirt-provider-ovn",
+            "passwordCredentials": {
+                "username": "admin@internal",
+                "password": "123"
+            }
+        }
+    }
+    response = requests.post(
+        OVN_PROVIDER_TOKEN_URL.format(
+            hostname=engine_name
+        ),
+        data=json.dumps(auth_request_data),
+        verify=False)
+    return response.json()
+
+
+def _get_auth_token(engine_name):
+    response_json = _request_auth_token(engine_name)
+    token_id = response_json['access']['token']['id']
+    return token_id
+
+
+def _get_networks(token_id, engine_name):
+    response = requests.get(
+        OVN_PROVIDER_NETWORKS_URL.format(
+            hostname=engine_name
+        ),
+        verify=False,
+        headers={
+            'X-Auth-Token': token_id
+        }
+    )
+    return response.json()
+
+
+@testlib.with_ovirt_prefix
+def test_ovn_provider_keystone_token(prefix):
+    engine = prefix.virt_env.engine_vm()
+    engine_ip = engine.ip()
+    response_json = _request_auth_token(engine_ip)
+    nt.assert_true(
+        lambda:
+        'access' in response_json and
+        'token' in response_json['access'] and
+        'id' in response_json['access']['token']
+    )
+    token_id = response_json['access']['token']['id']
+    try:
+        uuid.UUID(token_id)
+    except:
+        raise AssertionError(
+            'Incorrect provider auth token value: %s'
+            % (token_id)
+        )
+
+
+@testlib.with_ovirt_prefix
+def test_ovn_provider_networks(prefix):
+    engine = prefix.virt_env.engine_vm()
+    engine_ip = engine.ip()
+    token_id = _get_auth_token(engine_ip)
+    networks = _get_networks(token_id, engine_ip)
+    nt.assert_true(
+        'networks' in networks
+    )
+
+
+_TEST_LIST = [
+    test_ovn_provider_keystone_token,
+    test_ovn_provider_networks,
+]
+
+
+def test_gen():
+    for t in testlib.test_sequence_gen(_TEST_LIST):
+        test_gen.__name__ = t.description
+        yield t

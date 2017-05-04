7f9db42b83261ac1f83a87c9669a36f19aa420a8
Sahina Bose <sabose@redhat.com>
2017-05-04 11:05:32 -0400

    updated gdeploy version
    Fixes the compatibility issue with
    ansible-2.3
    
    Change-Id: I82e2f6b6ad8edf625b2e82eb1137987c8dc7bcf1
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- hc-basic-suite-master/gdeploy-setup.sh
+++ hc-basic-suite-master/gdeploy-setup.sh
@@ -3,5 +3,5 @@
 #Set up gdepl on the first node (host0), to deploy HC setup on host0, 1, 2
 # The reposync is not pickingup rpm from copr repo for some reason
 #yum install -y gdeploy
-yum install -y https://copr-be.cloud.fedoraproject.org/results/rnachimu/gdeploy/epel-7-x86_64/00521219-gdeploy/gdeploy-2.0.1-13.noarch.rpm
+yum install -y https://copr-be.cloud.fedoraproject.org/results/sac/gdeploy/epel-7-x86_64/00549451-gdeploy/gdeploy-2.0.2-7.noarch.rpm
 

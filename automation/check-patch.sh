e43192652b5cca5b2eb33cf46380f15904794829
gbenhaim <galbh2@gmail.com>
2017-08-09 19:19:09 +0300

    check-patch: Fail if an execution script is missing
    1. Fail check-patch if an execution script for a suite is missing.
       Masking this issue can lead to unreliable check-patch.
    
    2. Don't use "tput setaf 1" since it's not working inside mock.
    
    Change-Id: I2a65fcd48f4bffbe63f76604bf8b0016ee01e6b7
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -11,22 +11,22 @@ SUITES_TO_RUN=$(automation/change_resolver.py)
 [[ -z "$SUITES_TO_RUN" ]] && SUITES_TO_RUN="basic_suite_master"
 
 collect_suite_logs() {
-  # Each suite outputs its logs to a directory named "exported-artifacts".
-  # Since "check-patch" can run multiple suites, we need to rename
-  # "exported-artifacts" at the end of each suite
-  # (otherwise the logs will be overridden).
+    # Each suite outputs its logs to a directory named "exported-artifacts".
+    # Since "check-patch" can run multiple suites, we need to rename
+    # "exported-artifacts" at the end of each suite
+    # (otherwise the logs will be overridden).
 
-  # Convert the name of "exported-artifacts" directory to $1__logs
-  # $1: Suite name
+    # Convert the name of "exported-artifacts" directory to $1__logs
+    # $1: Suite name
 
-  local suite_name=${1:?}
-  local test_logs="${PROJECT}/exported-artifacts"
+    local suite_name=${1:?}
+    local test_logs="${PROJECT}/exported-artifacts"
 
-  if [[ -d "$test_logs" ]]; then
-    suite_logs="${PROJECT}/${suite_name}__logs"
-    # Rename the logs dir
-    mv "$test_logs" "$suite_logs"
-  fi
+    if [[ -d "$test_logs" ]]; then
+        suite_logs="${PROJECT}/${suite_name}__logs"
+        # Rename the logs dir
+        mv "$test_logs" "$suite_logs"
+    fi
 }
 
 collect_all_logs() {
@@ -39,11 +39,13 @@ collect_all_logs() {
     find . -maxdepth 1 -name '*.log' -print0 | xargs -r -0 mv -t exported-artifacts
 }
 
-# collect the logs  on failure
+on_exit() {
+    collect_all_logs
+}
+
 on_error() {
-  echo "Error on line: $1"
-  collect_suite_logs "$suite"
-  collect_all_logs
+    echo "Error on line: $1"
+    collect_suite_logs "$suite"
 }
 
 
@@ -54,8 +56,9 @@ if [[ -d "$PROJECT/exported-artifacts" ]]; then
     rm -rf "$PROJECT/exported-artifacts"
 fi
 
-#Trap any errors and print the line number
+# Trap any errors and print the line number
 trap 'on_error $LINENO' SIGTERM ERR
+trap 'on_exit' EXIT
 
 echo "Suites to run:"
 echo "${SUITES_TO_RUN}"
@@ -68,11 +71,9 @@ do
         echo "running $suite_exec_script"
         ./$suite_exec_script
     else
-        tput setaf 1; echo "!!! Could not find execution script for $suite"
+        echo "Could not find execution script for $suite"
+        exit 1
     fi
 
     collect_suite_logs "$suite"
 done
-
-# collect all the suit's logs to exported-artifacts
-collect_all_logs

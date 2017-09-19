91feacdafa897e15d2f20de29703df3742a06756
Eyal Edri <eedri@redhat.com>
2017-09-19 12:41:59 +0300

    Initial 4.2 suite ( using mostly 4.1 code )
     - we need to start working on 4.2 suite as more
       projects are brancing to 4.2 stable branch.
    
     - the main reason is that the CI system is built
       in away every change has to pass via CQ+OST
       in order to be deployed to tested/snapshot repos,
       so we need 4.2 suite for that.
    
     - using 4.1 as fallback repo for now, until we'll
       have more pkgs in 4.2 tested
    
     - using 4.1 tests for now until we'll have at least
       engine and vdsm built for 4.2, otherwise tests
       fail on 4.2 features
    
    Change-Id: I4fe6d1898ab1d0d8d82d3c4a7472247a786d84bf
    Signed-off-by: Eyal Edri <eedri@redhat.com>

---
+++ basic-suite-4.2/control.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+prep_suite () {
+    render_jinja_templates
+}
+
+run_suite () {
+    env_init \
+        "$1" \
+        "$SUITE/LagoInitFile"
+    env_repo_setup
+    install_local_rpms
+    env_start
+    env_status
+    if ! env_deploy; then
+        env_collect "$PWD/test_logs/${SUITE##*/}/post-000_deploy"
+        echo "@@@ ERROR: Failed in deploy stage"
+        return 1
+    fi
+    declare test_scenarios=($(ls "$SUITE"/test-scenarios/*.py | sort))
+    declare failed=false
+    for scenario in "${test_scenarios[@]}"; do
+        echo "Running test scenario ${scenario##*/}"
+        env_run_test "$scenario" || failed=true
+        env_collect "$PWD/test_logs/${SUITE##*/}/post-${scenario##*/}"
+        if $failed; then
+            echo "@@@@ ERROR: Failed running $scenario"
+            return 1
+        fi
+    done
+}

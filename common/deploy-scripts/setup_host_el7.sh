0a5a84bf64744caaf92444286379a6a985b036c5
Daniel Erez <derez@redhat.com>
2017-12-19 16:36:01 +0200

    Resereving port 54322 for imageio-daemon
    Similiary to the workaround done for imageio-proxy[1],
    reserve port 54322 for the imageio-daemon service prior
    to starting vdsm.
    
    [1] I76f632eb283c07cf23c857b3153916e50ebb1e67
    
    Change-Id: I885b28cac5970f4710fc721e8abebbe50579d605
    Bug-Url: https://bugzilla.redhat.com/1517764
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- common/deploy-scripts/setup_host_el7.sh
+++ common/deploy-scripts/setup_host_el7.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -xe
 
 yum update -y iptables
+
+# Reserving port 54322 for ovirt-imageio-daemon service
+# ToDo: this workaround can be removed once either of
+# the following bugs are resolved:
+# https://bugzilla.redhat.com/show_bug.cgi?id=1528971
+# https://bugzilla.redhat.com/show_bug.cgi?id=1528972
+sysctl -w net.ipv4.ip_local_reserved_ports=54322

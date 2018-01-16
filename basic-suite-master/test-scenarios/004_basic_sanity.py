b301376faab7b460da08abc0c3d0268a95ba42c9
Milan Zamazal <mzamazal@redhat.com>
2018-01-16 11:31:19 +0100

    Test snapshot with memory
    Let's introduce a test restoring from a snapshot with memory.  Note that we'd
    like to test not only that the restore operation works, but also that following
    common operations work too (at minimum migrations).  On the other hand some
    operations, such as disk hot plug, are not possible while previewing a
    snapshot.  So placement of the test matters from the view of tested
    functionality.
    
    This is just a basic version of the test to be extended in followup patches.
    Future versions of the test should check for instance that the right drive is
    used or (in a different suite) that restore from snapshot with memory works
    across cluster upgrades.
    
    Change-Id: Ibee6681ccfac191ab6a5fe6564bdde055f3623f6
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>

--- basic-suite-master/test-scenarios/004_basic_sanity.py
+++ basic-suite-master/test-scenarios/004_basic_sanity.py
@@ -74,6 +74,7 @@ VM_NETWORK = u'VM Network with a very long name and עברית'
 SNAPSHOT_DESC_1 = 'dead_snap1'
 SNAPSHOT_DESC_2 = 'dead_snap2'
 SNAPSHOT_FOR_BACKUP_VM = 'backup_snapshot'
+SNAPSHOT_DESC_MEM = 'memory_snap'
 
 VDSM_LOG = '/var/log/vdsm/vdsm.log'
 
@@ -452,6 +453,58 @@ def snapshot_cold_merge(api):
         lambda:
         vm1_snapshots_service.list()[-1].snapshot_status == types.SnapshotStatus.OK
     )
+
+
+@testlib.with_ovirt_api4
+def make_snapshot_with_memory(api):
+    engine = api.system_service()
+    vm_service = test_utils.get_vm_service(engine, VM0_NAME)
+    disks_service = engine.disks_service()
+    vm_disks_service = \
+        test_utils.get_disk_attachments_service(engine, VM0_NAME)
+    vm_disks = [disks_service.disk_service(attachment.disk.id).get()
+                for attachment in vm_disks_service.list()]
+    disk_attachments = [types.DiskAttachment(disk=types.Disk(id=disk.id))
+                        for disk in vm_disks
+                        if disk.storage_type != types.DiskStorageType.LUN]
+    snapshots_service = vm_service.snapshots_service()
+    snapshot_params = types.Snapshot(
+        description=SNAPSHOT_DESC_MEM,
+        persist_memorystate=True,
+        disk_attachments=disk_attachments
+    )
+    with test_utils.TestEvent(engine, 45):  # USER_CREATE_SNAPSHOT event
+        snapshots_service.add(snapshot_params)
+
+
+@testlib.with_ovirt_api4
+def preview_snapshot_with_memory(api):
+    engine = api.system_service()
+    vm_service = test_utils.get_vm_service(engine, VM0_NAME)
+
+    def snapshot_created():
+        snapshot = test_utils.get_snapshot(engine, VM0_NAME, SNAPSHOT_DESC_MEM)
+        return (snapshot is not None and
+                snapshot.snapshot_status == types.SnapshotStatus.OK)
+    testlib.assert_true_within_long(snapshot_created)
+    vm_service.stop()
+    _verify_vm_state(engine, VM0_NAME, types.VmStatus.DOWN)
+    snapshot = test_utils.get_snapshot(engine, VM0_NAME, SNAPSHOT_DESC_MEM)
+    vm_service.preview_snapshot(snapshot=snapshot, async=False,
+                                restore_memory=True)
+
+
+@testlib.with_ovirt_api4
+def check_snapshot_with_memory(api):
+    engine = api.system_service()
+    vm_service = test_utils.get_vm_service(engine, VM0_NAME)
+    testlib.assert_true_within_long(
+        lambda: test_utils.get_snapshot(engine, VM0_NAME,
+                                        SNAPSHOT_DESC_MEM).snapshot_status ==
+        types.SnapshotStatus.IN_PREVIEW
+    )
+    vm_service.start()
+    _verify_vm_state(engine, VM0_NAME, types.VmStatus.UP)
 
 
 @testlib.with_ovirt_api4
@@ -1199,10 +1252,13 @@ _TEST_LIST = [
     disk_operations,
     remove_vm2_lease,
     hotunplug_disk,
+    make_snapshot_with_memory,
     add_vm_pool,
+    preview_snapshot_with_memory,
     update_template_version,
     update_vm_pool,
     remove_vm_pool,
+    check_snapshot_with_memory,
     vdsm_recovery
 ]
 

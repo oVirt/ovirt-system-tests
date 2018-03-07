acc868e1b59614ce025507523aff6d317419cb83
Yuval Turgeman <yturgema@redhat.com>
2018-03-07 12:31:58 +0200

    image-ng: add a call to env_status
    Change-Id: Ie688c24613d1bf7a3fe1802c0c68c20296e94275
    Signed-off-by: Yuval Turgeman <yturgema@redhat.com>

--- image-ng-suite-master/control.sh
+++ image-ng-suite-master/control.sh
@@ -166,6 +166,7 @@ run_suite(){
         "http://templates.ovirt.org/repo/repo.metadata" \
         "$suite/LagoInitFile"
     env_start
+    env_status
     env_deploy
 
     declare test_scenarios=($(ls "$suite"/test-scenarios/*.py | sort))

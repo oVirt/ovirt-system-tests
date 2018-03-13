9a861d4483a1959e91a268295aefa2165d5cc545
Eyal Edri <eedri@redhat.com>
2018-03-13 10:52:14 +0200

    seperate 4.2 tests and utils from master
    master is starting to move towards 4.3 and new tests
    we need to start stabilizing 4.2 tests towards GA.
    
    Change-Id: I3ec7a355abd6f9653d69d039a09a9663798e22a9
    Signed-off-by: Eyal Edri <eedri@redhat.com>

---
+++ basic-suite-4.2/test-scenarios/003_basic_networking.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016-2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from lago import utils
+from netaddr.ip import IPAddress
+import nose.tools as nt
+from ovirtlago import testlib
+from ovirtsdk4.types import Bonding, HostNic, Option, VnicProfile, VnicPassThrough, VnicPassThroughMode
+
+import test_utils
+from test_utils import network_utils_v4
+
+
+# Environment (see control.sh and LagoInitFile.in)
+LIBVIRT_NETWORK_FOR_MANAGEMENT = testlib.get_prefixed_name('net-management')  # eth0
+LIBVIRT_NETWORK_FOR_BONDING = testlib.get_prefixed_name('net-bonding')  # eth2, eth3
+
+# DC/Cluster
+DC_NAME = 'test-dc'
+CLUSTER_NAME = 'test-cluster'
+
+# Networks
+VM_NETWORK = u'VM Network with a very long name and עברית'
+VM_NETWORK_IPv4_ADDR = '192.0.2.{}'
+VM_NETWORK_IPv4_MASK = '255.255.255.0'
+VM_NETWORK_IPv6_ADDR = '2001:0db8:85a3:0000:0000:8a2e:0370:733{}'
+VM_NETWORK_IPv6_MASK = '64'
+VM_NETWORK_VLAN_ID = 100
+
+MIGRATION_NETWORK = 'Migration_Net'  # MTU 9000
+BOND_NAME = 'bond0'
+MIGRATION_NETWORK_IPv4_ADDR = '192.0.3.{}'
+MIGRATION_NETWORK_IPv4_MASK = '255.255.255.0'
+MIGRATION_NETWORK_IPv6_ADDR = '1001:0db8:85a3:0000:0000:574c:14ea:0a0{}'
+MIGRATION_NETWORK_IPv6_MASK = '64'
+
+MANAGEMENT_NETWORK = 'ovirtmgmt'
+PASSTHROUGH_VNIC_PROFILE = 'passthrough_vnic_profile'
+
+
+def _host_vm_nics(prefix, host_name, network_name):
+    """
+    Return names of host VM NICs attached by Lago to a given libvirt network,
+    just like do_status in site-packages/lago/cmd.py:446
+
+    TODO: move 'eth{0}' magic to site-packages/ovirtlago/testlib.py?
+    """
+    lago_host_vm = prefix.get_vms()[host_name]
+    return ['eth{0}'.format(i) for i, nic in enumerate(lago_host_vm.nics())
+            if nic['net'] == network_name]
+
+
+def _ping(host, ip_address):
+    """
+    Ping a given address (IPv4/6) from a host.
+    """
+    cmd = ['ping', '-c', '1']
+    # TODO: support pinging by host names?
+    if ':' in ip_address:
+        cmd += ['-6']
+
+    ret = host.ssh(cmd + [ip_address])
+    nt.assert_equals(ret.code, 0, 'Cannot ping {} from {}: {}'.format(
+        ip_address, host.name(), ret))
+
+
+def _host_is_attached_to_network(engine, host, network_name, nic_name=None):
+    try:
+        attachment = network_utils_v4.get_network_attachment(
+            engine, host, network_name, DC_NAME)
+    except StopIteration:  # there is no attachment of the network to the host
+        return False
+
+    if nic_name:
+        host_nic = next(nic for nic in host.nics_service().list()
+                        if nic.id == attachment.host_nic.id)
+        nt.assert_equals(nic_name, host_nic.name)
+    return attachment
+
+
+@testlib.with_ovirt_api4
+@testlib.with_ovirt_prefix
+def attach_vm_network_to_host_0_static_config(prefix, api):
+    _attach_vm_network_to_host_static_config(prefix, api, host_num=0)
+
+
+@testlib.with_ovirt_api4
+@testlib.with_ovirt_prefix
+def attach_vm_network_to_host_1_static_config(prefix, api):
+    _attach_vm_network_to_host_static_config(prefix, api, host_num=1)
+
+
+def _attach_vm_network_to_host_static_config(prefix, api, host_num):
+    engine = api.system_service()
+
+    host = test_utils.hosts_in_cluster_v4(engine, CLUSTER_NAME)[host_num]
+    host_service = engine.hosts_service().host_service(id=host.id)
+
+    nic_name = _host_vm_nics(prefix, host.name,
+                             LIBVIRT_NETWORK_FOR_MANAGEMENT)[0]  # eth0
+    ip_configuration = network_utils_v4.create_static_ip_configuration(
+        VM_NETWORK_IPv4_ADDR.format(host_num+1),
+        VM_NETWORK_IPv4_MASK,
+        VM_NETWORK_IPv6_ADDR.format(host_num+1),
+        VM_NETWORK_IPv6_MASK)
+
+    network_utils_v4.attach_network_to_host(
+        host_service,
+        nic_name,
+        VM_NETWORK,
+        ip_configuration)
+
+    host_nic = next(nic for nic in host_service.nics_service().list() if
+                    nic.name == '{}.{}'.format(nic_name, VM_NETWORK_VLAN_ID))
+    nt.assert_equals(IPAddress(host_nic.ip.address),
+                     IPAddress(VM_NETWORK_IPv4_ADDR.format(host_num+1)))
+    nt.assert_equals(IPAddress(host_nic.ipv6.address),
+                     IPAddress(VM_NETWORK_IPv6_ADDR.format(host_num+1)))
+
+
+@testlib.with_ovirt_api4
+def modify_host_0_ip_to_dhcp(api):
+    engine = api.system_service()
+
+    host = test_utils.hosts_in_cluster_v4(engine, CLUSTER_NAME)[0]
+    host_service = engine.hosts_service().host_service(id=host.id)
+    ip_configuration = network_utils_v4.create_dhcp_ip_configuration()
+
+    network_utils_v4.modify_ip_config(engine, host_service, VM_NETWORK,
+                                      ip_configuration)
+
+    # TODO: once the VLANs/dnsmasq issue is resolved,
+    # (https://github.com/lago-project/lago/issues/375)
+    # verify ip configuration.
+
+
+@testlib.with_ovirt_api4
+def detach_vm_network_from_host_0(api):
+    engine = api.system_service()
+
+    host = test_utils.hosts_in_cluster_v4(engine, CLUSTER_NAME)[0]
+    host_service = engine.hosts_service().host_service(id=host.id)
+
+    network_utils_v4.set_network_required_in_cluster(
+        engine, VM_NETWORK, CLUSTER_NAME, False)
+    network_utils_v4.detach_network_from_host(engine, host_service, VM_NETWORK)
+
+    nt.assert_false(_host_is_attached_to_network(engine, host_service,
+                                                 VM_NETWORK))
+
+
+@testlib.with_ovirt_api4
+@testlib.with_ovirt_prefix
+def bond_nics(prefix, api):
+    engine = api.system_service()
+
+    def _bond_nics(number, host):
+        slaves = [HostNic(name=nic) for nic in _host_vm_nics(  # eth2, eth3
+                    prefix, host.name, LIBVIRT_NETWORK_FOR_BONDING)]
+
+        options = [
+            Option(name='mode', value='active-backup'),
+            Option(name='miimon', value='200'),
+            ]
+
+        bond = HostNic(
+            name=BOND_NAME,
+            bonding=Bonding(slaves=slaves, options=options))
+
+        ip_configuration = network_utils_v4.create_static_ip_configuration(
+            MIGRATION_NETWORK_IPv4_ADDR.format(number),
+            MIGRATION_NETWORK_IPv4_MASK,
+            MIGRATION_NETWORK_IPv6_ADDR.format(number),
+            MIGRATION_NETWORK_IPv6_MASK)
+
+        host_service = engine.hosts_service().host_service(id=host.id)
+        network_utils_v4.attach_network_to_host(
+            host_service, BOND_NAME, MIGRATION_NETWORK, ip_configuration,
+            [bond])
+
+    hosts = test_utils.hosts_in_cluster_v4(engine, CLUSTER_NAME)
+    utils.invoke_in_parallel(_bond_nics, range(1, len(hosts) + 1), hosts)
+
+    for host in test_utils.hosts_in_cluster_v4(engine, CLUSTER_NAME):
+        host_service = engine.hosts_service().host_service(id=host.id)
+        nt.assert_true(_host_is_attached_to_network(
+            engine, host_service, MIGRATION_NETWORK, nic_name=BOND_NAME))
+
+
+@testlib.with_ovirt_prefix
+def verify_interhost_connectivity_ipv4(prefix):
+    first_host = prefix.virt_env.host_vms()[0]
+    _ping(first_host, MIGRATION_NETWORK_IPv4_ADDR.format(2))
+
+
+@testlib.with_ovirt_prefix
+def verify_interhost_connectivity_ipv6(prefix):
+    first_host = prefix.virt_env.host_vms()[0]
+    _ping(first_host, MIGRATION_NETWORK_IPv6_ADDR.format(2))
+
+
+@testlib.with_ovirt_api4
+def remove_bonding(api):
+    engine = api.system_service()
+
+    def _remove_bonding(host):
+        host_service = engine.hosts_service().host_service(id=host.id)
+        network_utils_v4.detach_network_from_host(
+            engine, host_service, MIGRATION_NETWORK, BOND_NAME)
+
+    network_utils_v4.set_network_required_in_cluster(engine, MIGRATION_NETWORK,
+                                                     CLUSTER_NAME, False)
+    utils.invoke_in_parallel(
+        _remove_bonding, test_utils.hosts_in_cluster_v4(engine, CLUSTER_NAME))
+
+    for host in test_utils.hosts_in_cluster_v4(engine, CLUSTER_NAME):
+        host_service = engine.hosts_service().host_service(id=host.id)
+        nt.assert_false(_host_is_attached_to_network(engine, host_service,
+                                                     MIGRATION_NETWORK))
+
+
+def _get_vnic_service(engine, network_name):
+    networks_service = engine.networks_service()
+    net = networks_service.list(search='name={}'.format(network_name))[0]
+    vnic_service = networks_service.network_service(net.id).vnic_profiles_service()
+    return vnic_service
+
+
+@testlib.with_ovirt_api4
+def add_vnic_passthrough_profile(api):
+    engine = api.system_service()
+
+    vnic_service = _get_vnic_service(engine, MANAGEMENT_NETWORK)
+
+    vnic_profile = vnic_service.add(
+        profile=VnicProfile(name=PASSTHROUGH_VNIC_PROFILE,
+                            pass_through=VnicPassThrough(mode=VnicPassThroughMode.ENABLED)))
+    nt.assert_equals(vnic_profile.pass_through.mode, VnicPassThroughMode.ENABLED)
+
+
+@testlib.with_ovirt_api4
+def remove_vnic_passthrough_profile(api):
+    engine = api.system_service()
+
+    vnic_service = _get_vnic_service(engine, MANAGEMENT_NETWORK)
+
+    vnic_profile = next(vnic_profile for vnic_profile in vnic_service.list()
+                        if vnic_profile.name == PASSTHROUGH_VNIC_PROFILE
+                        )
+
+    vnic_service.profile_service(vnic_profile.id).remove()
+    nt.assert_equals(next((vnic_profile for vnic_profile in vnic_service.list()
+                           if vnic_profile.name == PASSTHROUGH_VNIC_PROFILE), None),
+                     None)
+
+
+_TEST_LIST = [
+    attach_vm_network_to_host_0_static_config,
+    modify_host_0_ip_to_dhcp,
+    detach_vm_network_from_host_0,
+    bond_nics,
+    verify_interhost_connectivity_ipv4,
+    verify_interhost_connectivity_ipv6,
+    remove_bonding,
+
+    # preparation for 004 and 006
+    attach_vm_network_to_host_0_static_config,
+    attach_vm_network_to_host_1_static_config,
+
+    add_vnic_passthrough_profile,
+    remove_vnic_passthrough_profile,
+]
+
+
+def test_gen():
+    for t in testlib.test_sequence_gen(_TEST_LIST):
+        test_gen.__name__ = t.description
+        yield t
---
+++ basic-suite-4.2/test_utils/__init__.py
@@ -0,0 +1,160 @@
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import collections
+import functools
+
+# Taken from https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+class memoized(object):
+   '''Decorator. Caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned
+   (not reevaluated).
+   '''
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      if not isinstance(args, collections.Hashable):
+         # uncacheable. a list, for instance.
+         # better to not cache than blow up.
+         return self.func(*args)
+      if args in self.cache:
+         return self.cache[args]
+      else:
+         value = self.func(*args)
+         self.cache[args] = value
+         return value
+   def __repr__(self):
+      '''Return the function's docstring.'''
+      return self.func.__doc__
+   def __get__(self, obj, objtype):
+      '''Support instance methods.'''
+      return functools.partial(self.__call__, obj)
+
+
+@memoized
+def get_nics_service(engine, vm_name):
+    vm_service = get_vm_service(engine, vm_name)
+    nics_service = vm_service.nics_service()
+    return nics_service
+
+
+def get_network_fiter_parameters_service(engine, vm_name):
+    nics_service = get_nics_service(engine, vm_name)
+    nic = nics_service.list()[0]
+    return nics_service.nic_service(id=nic.id)\
+        .network_filter_parameters_service()
+
+
+@memoized
+def get_vm_service(engine, vm_name):
+    vms_service = engine.vms_service()
+    vm = vms_service.list(search='name={}'.format(vm_name))[0]
+    if vm is None:
+        return None
+    return vms_service.vm_service(vm.id)
+
+
+@memoized
+def get_disk_service(engine, disk_name):
+    disks_service = engine.disks_service()
+    disk = disks_service.list(search='name={}'.format(disk_name))[0]
+    return disks_service.disk_service(disk.id)
+
+
+@memoized
+def get_disk_attachments_service(engine, vm_name):
+    vm_service = get_vm_service(engine, vm_name)
+    if vm_service is None:
+        return None
+    return vm_service.disk_attachments_service()
+
+
+@memoized
+def get_template_service(engine, template_name):
+    templates_service = engine.templates_service()
+    template = templates_service.list(search='name={}'.format(template_name))[0]
+    if template is None:
+        return None
+    return templates_service.template_service(template.id)
+
+
+@memoized
+def get_pool_service(engine, pool_name):
+    vm_pools_service= engine.vm_pools_service()
+    pool = vm_pools_service.list(search='name={}'.format(pool_name))[0]
+    return vm_pools_service.pool_service(pool.id)
+
+
+@memoized
+def get_storage_domain_service(engine, sd_name):
+    storage_domains_service = engine.storage_domains_service()
+    sd = storage_domains_service.list(search='name={}'.format(sd_name))[0]
+    return storage_domains_service.storage_domain_service(sd.id)
+
+
+def get_storage_domain_vm_service_by_name(sd_service, vm_name):
+    vms_service = sd_service.vms_service()
+    # StorageDomainVmsService.list has no 'search' parameter and ignores
+    # query={'name': 'spam'} so we have to do the filtering ourselves
+    vm = next((vm for vm in vms_service.list() if vm.name == vm_name), None)
+    if vm is None:
+        return None
+    else:
+        return vms_service.vm_service(vm.id)
+
+
+def hosts_in_cluster_v4(root, cluster_name):
+    hosts = root.hosts_service().list(search='cluster={}'.format(cluster_name))
+    return sorted(hosts, key=lambda host: host.name)
+
+
+@memoized
+def data_center_service(root, name):
+    data_centers = root.data_centers_service()
+    dc = data_centers.list(search='name={}'.format(name))[0]
+    return data_centers.data_center_service(dc.id)
+
+
+@memoized
+def get_cluster_service(engine, cluster_name):
+    clusters_service = engine.clusters_service()
+    cluster = clusters_service.list(search='name={}'.format(cluster_name))[0]
+    return clusters_service.cluster_service(cluster.id)
+
+
+@memoized
+def get_vm_snapshots_service(engine, vm_name):
+    vm_service = get_vm_service(engine, vm_name)
+    if vm_service is None:
+        return None
+    return vm_service.snapshots_service()
+
+
+def quote_search_string(s):
+    # TODO: this function should eventually be able to format strings in
+    # a way that they will be properly passed on via the sdk to Engine.
+    #
+    # Escaped characters are currently broken, but strings containing spaces
+    # are able to be passed with enclosing quotation marks.
+    if '"' in s:
+        raise ValueError(
+            'Quotation marks currently can not be appear in search phrases')
+    return '"' + s + '"'

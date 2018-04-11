1d788ec3a61fc9549bf57ec8777346a8fb4ed79a
Yaniv Kaul <ykaul@redhat.com>
2018-04-11 10:47:14 +0300

    Set (in runtime) 3 2M hugepages per node per host.
    For now, it's not usable expect for seeing it reported in the
    Engine.
    In the future, we might use it to run VMs with hugepages.
    
    Note that it is set in runtime, so if the host is rebooted,
    those pages will not be set.
    
    Change-Id: I32e1bc9c4b5fd240be97275c97079ac6585fbcaf

--- common/deploy-scripts/setup_host_el7.sh
+++ common/deploy-scripts/setup_host_el7.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xe
 set -ex
 
+HUGEPAGES=3
+
 yum update -y iptables
 
 # Reserving port 54322 for ovirt-imageio-daemon service
@@ -9,3 +11,7 @@ yum update -y iptables
 # https://bugzilla.redhat.com/show_bug.cgi?id=1528971
 # https://bugzilla.redhat.com/show_bug.cgi?id=1528972
 sysctl -w net.ipv4.ip_local_reserved_ports=54322
+
+for node in /sys/devices/system/node/node*; do
+    echo $HUGEPAGES > $node/hugepages/hugepages-2048kB/nr_hugepages;
+done

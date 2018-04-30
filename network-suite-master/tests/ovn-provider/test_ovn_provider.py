1bc1f07587ccc991febbd149e68c90d27f9c3b3f
Marcin Mirecki <mmirecki@redhat.com>
2018-04-30 15:14:12 +0200

    network: add test validating connections between OVN  networks
    This patch test the connection between OVN networks.
    It adds (and deletes afterwards) internal ovs network
    ports, and binds them to OVN logical ports.
    These ovs ports are then used to test connectivity
    between the ovn ports.
    
    Note that this is a temporary solution until we are
    able to ping via the deployed vms.
    
    Change-Id: I4ca08e609d67e920664e6ef2ce5a31b47c7e1f4d
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>

--- network-suite-master/tests/ovn-provider/test_ovn_provider.py
+++ network-suite-master/tests/ovn-provider/test_ovn_provider.py
@@ -17,14 +17,19 @@
 #
 # Refer to the README and COPYING files for full details of the license
 #
+from contextlib import contextmanager
 import os
 
-
 from lib.ansiblelib import Playbook
+from lib import sshlib as ssh
 from testlib import suite
 
 
 PLAYBOOK_DIR = os.path.join(os.environ.get('SUITE'), 'ansible')
+NETWORK10_PORT1_NAME = 'net10_port1'
+NETWORK11_PORT1_NAME = 'net11_port1'
+NETWORK10_SUBNET1_NAME = 'net10_subnet1'
+NETWORK11_SUBNET1_NAME = 'net11_subnet1'
 
 
 pytestmark = suite.SKIP_SUITE_42
@@ -36,6 +41,26 @@ class HostConfigurationFailure(Exception):
 
 def test_ovn_provider_create_scenario(openstack_client_config):
     _test_ovn_provider('create_scenario.yml')
+
+
+def test_validate_ovn_provider_connectivity(default_ovn_provider_client,
+                                            host_0, host_1):
+    port1 = default_ovn_provider_client.get_port(NETWORK10_PORT1_NAME)
+    port2 = default_ovn_provider_client.get_port(NETWORK11_PORT1_NAME)
+
+    subnet1 = default_ovn_provider_client.get_subnet(NETWORK10_SUBNET1_NAME)
+    subnet2 = default_ovn_provider_client.get_subnet(NETWORK11_SUBNET1_NAME)
+
+    connections = ((host_0, port1, subnet1,), (host_1, port2, subnet2,),)
+    with _create_namespaces(connections):
+        with _create_ovs_ports(connections):
+            ssh.exec_command(host_0.address, host_0.root_password,
+                             'ip netns exec ' + port1.name + ' ping -4 -c 1 ' +
+                             port2.fixed_ips[0]['ip_address'])
+
+            ssh.exec_command(host_1.address, host_1.root_password,
+                             'ip netns exec ' + port2.name + ' ping -4 -c 1 ' +
+                             port1.fixed_ips[0]['ip_address'])
 
 
 def test_ovn_provider_cleanup_scenario(openstack_client_config):
@@ -51,3 +76,77 @@ def _test_ovn_provider(playbook_name):
     assert not playbook.idempotency_check_stats.failures
     assert playbook.execution_stats.changed.get('localhost', 0) > 0
     assert not playbook.idempotency_check_stats.changed
+
+
+@contextmanager
+def _create_namespaces(connections):
+    namespaces = list()
+    try:
+        for host, port, _ in connections:
+            _exec(host, _add_namespace_command(port.name))
+            namespaces.append((host, port.name))
+        yield
+    finally:
+        for host, name in namespaces:
+            _exec(host, _delete_namespace_command(name))
+
+
+@contextmanager
+def _create_ovs_ports(connections):
+    ports = list()
+    try:
+        for host, port, _ in connections:
+            _exec(host, _add_ovs_port_command(port.name))
+            ports.append((host, port.name))
+        for host, port, subnet in connections:
+            _exec(host, _configure_ovs_port_command(port, subnet) +
+                  _bind_port_to_logical_network(port))
+        yield
+    finally:
+        for host, name in ports:
+            _exec(host, _delete_ovs_port_command(name))
+
+
+def _exec(host, commands):
+    ssh.exec_command(host.address, host.root_password,
+                     ' && '.join(commands))
+
+
+def _add_namespace_command(name):
+    return ['ip netns add ' + name]
+
+
+def _add_ovs_port_command(name):
+    commands = [
+        'ovs-vsctl add-port br-int ' + name + ' -- set interface ' + name +
+        ' type=internal'
+    ]
+    return commands
+
+
+def _configure_ovs_port_command(port, subnet):
+    name = port.name
+    ip = port.fixed_ips[0]['ip_address']
+    mac = port.mac_address
+    gw = subnet.gateway_ip
+    commands = [
+        'ip link set ' + name + ' netns ' + name,
+        'ip netns exec ' + name + ' ip link set ' + name + ' address ' + mac,
+        'ip netns exec ' + name + ' ip addr add ' + ip + '/24 dev ' + name,
+        'ip netns exec ' + name + ' ip link set ' + name + ' up',
+        'ip netns exec ' + name + ' ip route add default via ' + gw,
+    ]
+    return commands
+
+
+def _bind_port_to_logical_network(port):
+    return ['ovs-vsctl set Interface ' + port.name +
+            ' external_ids:iface-id=' + port.id]
+
+
+def _delete_namespace_command(name):
+    return ['ip netns delete ' + name]
+
+
+def _delete_ovs_port_command(name):
+    return ['ovs-vsctl del-port ' + name]

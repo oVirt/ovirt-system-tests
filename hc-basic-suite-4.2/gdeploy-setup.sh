306d506eaaed17b926406906facb1d05dbac57b6
Galit Rosenthal <grosenth@redhat.com>
2019-06-12 12:20:08 +0300

    Remove hc-basic-suite-4.2 suite
    Change-Id: If1cae8fbce3638a4cb84b5de970b86e430950db5
    Signed-off-by: Galit Rosenthal <grosenth@redhat.com>

--- hc-basic-suite-4.2/gdeploy-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#Set up gdepl on the first node (host0), to deploy HC setup on host0, 1, 2
-# The reposync is not pickingup rpm from copr repo for some reason
-yum install -y gdeploy

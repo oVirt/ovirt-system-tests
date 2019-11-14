546d46dee4903cc040d79beb8421d8e9348152c1
Ori_Liel <oliel@redhat.com>
2019-11-14 14:16:25 +0200

    Remove Import_Templates_Test
    This deprecated test is unnecessary anymore, this
    patch removes it.
    
    Change-Id: I0a2eb90f80f2040bacd7b64cedf3f7f485ec66a7
    Signed-off-by: Ori_Liel <oliel@redhat.com>

--- basic-suite-master/test-scenarios/002_bootstrap.py
+++ basic-suite-master/test-scenarios/002_bootstrap.py
@@ -613,32 +613,6 @@ def add_iso_storage_domain(prefix):
 def add_templates_storage_domain(prefix):
     add_generic_nfs_storage_domain(prefix, SD_TEMPLATES_NAME, SD_TEMPLATES_HOST_NAME, SD_TEMPLATES_PATH, sd_format='v1', sd_type='export', nfs_version='v4_1')
 
-
-@testlib.with_ovirt_api
-def import_templates(api):
-    #TODO: Fix the exported domain generation
-    raise SkipTest('Exported domain generation not supported yet')
-    templates = api.storagedomains.get(
-        SD_TEMPLATES_NAME,
-    ).templates.list(
-        unregistered=True,
-    )
-
-    for template in templates:
-        template.register(
-            action=params.Action(
-                cluster=params.Cluster(
-                    name=CLUSTER_NAME,
-                ),
-            ),
-        )
-
-    for template in api.templates.list():
-        testlib.assert_true_within_short(
-            lambda: api.templates.get(template.name).status.state == 'ok',
-        )
-
-
 def generic_import_from_glance(glance_provider, as_template=False,
                                dest_storage_domain=MASTER_SD_TYPE,
                                dest_cluster=CLUSTER_NAME):
@@ -1921,7 +1895,6 @@ _TEST_LIST = [
     add_secondary_storage_domains,
     resize_and_refresh_storage_domain,
     add_vm2_lease,
-    import_templates,
     add_non_vm_network,
     add_vm_network,
 ]

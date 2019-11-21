01c3ea27cbc7cb742e3155343e34745e107ac9ba
Galit Rosenthal <grosenth@redhat.com>
2019-11-21 12:23:49 +0200

    copy sdk4:add_quota_cluster_limits from master
    Change-Id: Ib73be5c4dbf45382039eec79a18d192dffb2181e
    Signed-off-by: Galit Rosenthal <grosenth@redhat.com>

--- performance-suite-master/test-scenarios/040_add_hosts_vms.py
+++ performance-suite-master/test-scenarios/040_add_hosts_vms.py
@@ -528,13 +528,26 @@ def add_quota_storage_limits(api):
     )
 
 
-@testlib.with_ovirt_api
+@testlib.with_ovirt_api4
 def add_quota_cluster_limits(api):
-    dc = api.datacenters.get(DC_NAME)
-    quota = dc.quotas.get(name=DC_QUOTA_NAME)
-    quota_cluster = params.QuotaClusterLimit(vcpu_limit=20, memory_limit=10000)
+    datacenters_service = api.system_service().data_centers_service()
+    datacenter = datacenters_service.list(search='name=%s' % DC_NAME)[0]
+    datacenter_service = datacenters_service.data_center_service(datacenter.id)
+    quotas_service = datacenter_service.quotas_service()
+    quotas = quotas_service.list()
+    quota = next(
+        (q for q in quotas if q.name == DC_QUOTA_NAME),
+        None
+    )
+    quota_service = quotas_service.quota_service(quota.id)
+    quota_cluster_limits_service = quota_service.quota_cluster_limits_service()
     nt.assert_true(
-        quota.quotaclusterlimits.add(quota_cluster)
+        quota_cluster_limits_service.add(
+            types.QuotaClusterLimit(
+                vcpu_limit=20,
+                memory_limit=10000.0
+            )
+        )
     )
 
 

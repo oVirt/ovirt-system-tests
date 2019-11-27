86205b422498a3122e428f241a3f9094f2d0fd40
Galit Rosenthal <grosenth@redhat.com>
2019-11-27 11:43:17 +0200

    Performance: Remove import to sdk3
    Change-Id: I16641c4bb9ea99dc455f0f58def06590760c6f92
    Signed-off-by: Galit Rosenthal <grosenth@redhat.com>

--- performance-suite-master/test-scenarios/040_add_hosts_vms.py
+++ performance-suite-master/test-scenarios/040_add_hosts_vms.py
@@ -23,7 +23,6 @@ import random
 
 import nose.tools as nt
 from nose import SkipTest
-from ovirtsdk.xml import params
 
 # TODO: import individual SDKv4 types directly (but don't forget sdk4.Error)
 import ovirtsdk4 as sdk4

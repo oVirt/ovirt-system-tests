80bb304401fc7eb85fb21a4a5375969a38ff41d6
Martin Perina <mperina@redhat.com>
2020-01-03 15:49:37 +0100

    he master: Copy add_labeled_network from basic master
    Change-Id: I00028ae46c7ce9208beea5c2b1957795bb8ff5d1
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- he-basic-suite-master/test-scenarios/006_network_by_label.py
+++ he-basic-suite-master/test-scenarios/006_network_by_label.py
@@ -77,43 +77,44 @@ def assign_hosts_network_label(api):
     nt.assert_true(all(vt.join_all()))
 
 
-@testlib.with_ovirt_api
+@testlib.with_ovirt_api4
 def add_labeled_network(api):
     """
     Creates a labeled network
     """
-
     # create network
-    labeled_net = params.Network(
+    labeled_net = Network(
         name=LABELED_NET_NAME,
-        data_center=params.DataCenter(
+        data_center=DataCenter(
             name=DC_NAME,
         ),
         description='Labeled network on VLAN {}'.format(LABELED_NET_VLAN_ID),
-        usages=params.Usages(),
-        # because only one non-VLAN network, here 'ovirtmgmt', can be assigned to each nic,
-        # this additional network has to be a VLAN network
-        vlan=params.VLAN(
+        usages=[],
+        # because only one non-VLAN network, here 'ovirtmgmt', can be assigned
+        # to each nic, this additional network has to be a VLAN network
+        # NOTE: we have added three more NICs since creating this test
+        vlan=Vlan(
             id=LABELED_NET_VLAN_ID,
         ),
     )
-    net = api.networks.add(labeled_net)
-    nt.assert_true( net )
+    networks_service = api.system_service().networks_service()
+    net = networks_service.add(labeled_net)
+    nt.assert_true(net)
+
+    network_service = networks_service.network_service(id=net.id)
+    labels_service = network_service.network_labels_service()
 
     # assign label to the network
     nt.assert_true(
-        net.labels.add(
-            params.Label(
+        labels_service.add(
+            NetworkLabel(
                 id=NETWORK_LABEL
             )
         )
     )
     nt.assert_equal(
-        len(
-            net.labels.list(
-                id=NETWORK_LABEL
-            )
-        ),
+        len(list(label for label in labels_service.list()
+                 if label.id == NETWORK_LABEL)),
         1
     )
 

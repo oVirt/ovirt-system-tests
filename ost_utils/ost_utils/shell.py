91d3f386c9e1089ccc2ee5586cfeb1195eb25b08
Marcin Sobczyk <msobczyk@redhat.com>
2020-03-13 21:00:24 +0100

    ost_utils: Added 'shell' module
    Added a module that helps launching simple commands. When the command
    executed succeeds, it returns its stdout. When it fails, it raises
    a 'ShellError' exception that contains the return code, stdout and
    stderr.
    
    Change-Id: Ia5cf0a902d77e073e090db4c20de66c6875b65ee
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

---
+++ ost_utils/ost_utils/shell.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import subprocess
+
+
+class ShellError(Exception):
+
+    def __init__(self, code, out, err):
+        self.code = code
+        self.out = out
+        self.err = err
+
+
+def shell(args, bytes_output=False):
+    process = subprocess.Popen(args,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, err = process.communicate()
+
+    if not bytes_output:
+        out = out.decode("utf-8")
+        err = err.decode("utf-8")
+
+    if process.returncode:
+        raise ShellError(process.returncode, out, err)
+
+    return out

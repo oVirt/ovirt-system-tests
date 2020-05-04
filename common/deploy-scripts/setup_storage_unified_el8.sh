oVirt/ovirt-system-tests

commit d47e58dfbcdb0bce1655cadefee4e5f1f49134ea

optimize package installation
Change-Id: I44e0cd8202109c69d71a875633c18e1ae8694d00
Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

common/deploy-scripts/setup_storage_unified_el8.sh
@@ -54,14 +54,26 @@ set_selinux_on_nfs() {
 
 install_deps() {
     systemctl disable --now kdump.service
-    yum install --nogpgcheck -y \
-                   nfs-utils \
-                   rpcbind \
-                   lvm2 \
-                   targetcli \
-                   sg3_utils \
-                   iscsi-initiator-utils \
-                   policycoreutils-python-utils
+    pkgs_to_install=(
+    "net-snmp"
+    "ovirt-engine"
+    "ovirt-log-collector"
+    "ovirt-engine-extension-aaa-ldap-setup"
+    "otopi-debug-plugins"
+    "cronie"
+    "nfs-utils"
+    "rpcbind"
+    "lvm2"
+    "targetcli"
+    "sg3_utils"
+    "iscsi-initiator-utils"
+    "policycoreutils-python-utils"
+    )
+    rpm -q "${pkgs_to_install[@]}" >/dev/null || yum install --nogpgcheck -y "${pkgs_to_install[@]}" || {
+        ret=$?
+        echo "install failed with status $ret"
+        exit $ret
+    }
 }
 
 

commit c83ba3f4ea4efc5bbbb367d8df1133a367eec7b8

fix list of packages for storage setup
commit d47e58dfbcdb0bce1655cadefee4e5f1f49134ea accidentally added
ovirt-engine packages to the list of things to install, but that's not
correct for suites using dedicated storage VM

Change-Id: I260bda0ef2ab17f177f7ed569a3d5dcd2053ebfa
Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

common/deploy-scripts/setup_storage_unified_el8.sh
@@ -55,12 +55,6 @@ set_selinux_on_nfs() {
 install_deps() {
     systemctl disable --now kdump.service
     pkgs_to_install=(
-    "net-snmp"
-    "ovirt-engine"
-    "ovirt-log-collector"
-    "ovirt-engine-extension-aaa-ldap-setup"
-    "otopi-debug-plugins"
-    "cronie"
     "nfs-utils"
     "rpcbind"
     "lvm2"
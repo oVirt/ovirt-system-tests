1d1614ae0e3789bfdabfd1769056461787ca8307
Marcin Sobczyk <msobczyk@redhat.com>
2020-05-26 16:10:24 +0200

    ost-images: Use -f and -F to define image format
    This patch adds '-f qcow2' and '-F qcow2' options when using
    qemu-img to define the format of the images explicitly.
    
    Change-Id: Ia57b7cdd12b318d3d96938146915ae753ec7c41c
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- ost-images/rebase_images.sh
+++ ost-images/rebase_images.sh
@@ -5,12 +5,12 @@ IMGDIR=${1:-`pwd`}
 pushd "${IMGDIR}"
 
 for qcow in *.qcow2; do
-    backing="$(qemu-img info ${qcow} | sed -n 's/backing file: \([^[:space:]]*\).*/\1/p')"
+    backing="$(qemu-img info -f qcow2 ${qcow} | sed -n 's/backing file: \([^[:space:]]*\).*/\1/p')"
 
     if [ -n "${backing}" ]; then
         backing="${IMGDIR}/$(basename ${backing})"
         echo "Rebasing ${qcow} to ${backing}"
-        qemu-img rebase -u -b "${backing}" "${qcow}"
+        qemu-img rebase -f qcow2 -F qcow2 -u -b "${backing}" "${qcow}"
     else
         echo "${qcow} has no base image, skipping"
     fi

144e70075739f8032d387e9ee8c960704ad99be4
Marcin Sobczyk <msobczyk@redhat.com>
2020-05-29 19:01:35 +0200

    ost_utils: Move 'memoized' from 'test_utils' to 'ost_utils'
    'test_utils' are basic-suite-specific, while the 'memoized' decorator
    is very helpful in many cases. Moving to 'ost_utils' for broader availability.
    
    Change-Id: Iebb5fb978a628d15ea4685ac50eee55457a03121
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- basic-suite-master/test_utils/__init__.py
+++ basic-suite-master/test_utils/__init__.py
@@ -18,7 +18,6 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
-import collections
 import contextlib
 import functools
 import os
@@ -28,6 +27,7 @@ import re
 import six
 import uuid
 from ovirtlago import testlib
+from ost_utils.memoized import memoized
 from test_utils.constants import VM0_IP_HOST_PART
 
 
@@ -67,37 +67,6 @@ def test_gen(tests, generator):
             test_id = '{}_{}'.format(test_id, ost_dc_version.replace('.', ''))
         generator.__name__ = test_id
         yield t
-
-
-# Taken from https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
-class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        if not isinstance(args, collections.Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
 
 
 @memoized
---
+++ ost_utils/ost_utils/memoized.py
@@ -0,0 +1,55 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import collections
+import functools
+
+
+# Taken from https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+class memoized(object):
+    '''Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
+
+

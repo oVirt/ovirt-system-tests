551612f35a5befa64c608caec5b0d8552b07256f
Michal Skrivanek <michal.skrivanek@redhat.com>
2020-07-30 08:02:04 +0200

    add back vm0 constants due to 4.3 dependencies
    partial revert of 448798745e8a8e6f05cac0f4258392bbef8359a9 for 4.3
    HE suites
    
    Change-Id: I14cc60fe5824686481e2b596e3e1389224b39789
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- basic-suite-master/test_utils/__init__.py
+++ basic-suite-master/test_utils/__init__.py
@@ -28,6 +28,7 @@ import six
 import uuid
 from ovirtlago import testlib
 from ost_utils.memoized import memoized
+from test_utils.constants import VM0_IP_HOST_PART
 
 
 @testlib.with_ovirt_api4
@@ -279,6 +280,12 @@ def get_luns(prefix, host, port, target, from_lun, to_lun=None):
             luns.append(lun)
 
     return luns
+
+
+# unused in master, but sadly this is shared with he-basic-role-remote_suite_4.3, he-basic_suite_4.3, he-basic-iscsi_suite_4.3
+def get_vm0_ip_address(prefix):
+    gw_address = get_management_net(prefix).gw()
+    return '.'.join(gw_address.split('.')[0:3] + [VM0_IP_HOST_PART])
 
 
 def get_management_net(prefix):
--- basic-suite-master/test_utils/constants.py
+++ basic-suite-master/test_utils/constants.py
@@ -21,3 +21,5 @@
 DEFAULT_OVN_PROVIDER_NAME = 'ovirt-provider-ovn'
 ADD_HOST_TIMEOUT = 20 * 60
 ENGINE_VM_RESTART_TIMEOUT = 20 * 60
+# unused in master, but sadly this is shared with he-basic-role-remote_suite_4.3, he-basic_suite_4.3, he-basic-iscsi_suite_4.3
+VM0_IP_HOST_PART = '90'

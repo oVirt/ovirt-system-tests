c678c9462cfdf20a258467604cb4ad7edd543e60
Marcin Sobczyk <msobczyk@redhat.com>
2020-08-17 14:57:25 +0200

    basic: performance: decouple 'control.sh'
    Until now 'control.sh' file was shared between performance suite
    and basic suite. Since performance suite is not 100% ported to pytest
    yet, the script had to support both running tests modules with pytest
    and the legacy way. This patch decouples 'control.sh' by changing
    the symlink to a copy so we can move on with basic suite.
    When performance suite is ported to pytest this patch can be reverted.
    
    Change-Id: Ie19edeea8c3a7f8741f8ba2ec44f7b1afd673847
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- performance-suite-master/control.sh
+++ performance-suite-master/control.sh
@@ -1,1 +1,59 @@
-../basic-suite-master/control.sh
+#!/usr/bin/env bash
+
+prep_suite () {
+    render_jinja_templates
+}
+
+run_suite () {
+    install_libguestfs
+    env_init \
+        "$1" \
+        "$SUITE/LagoInitFile"
+    env_repo_setup
+    put_host_image
+    install_local_rpms_without_reposync
+    env_start
+    env_dump_ansible_hosts
+    env_copy_repo_file
+    env_copy_config_file
+    env_status
+    cd "$OST_REPO_ROOT"
+    if ! env_deploy; then
+        env_collect "$PWD/test_logs/${SUITE##*/}/post-000_deploy"
+        echo "@@@ ERROR: Failed in deploy stage"
+        return 1
+    fi
+    declare test_scenarios=($(ls "$SUITE"/test-scenarios/*.py | grep -v conftest | sort))
+    declare failed=false
+
+    cd "$OST_REPO_ROOT" && "${PYTHON}" -m pip install --user -e ost_utils
+    "${PYTHON}" -m pip install --user -I selenium || echo "ERROR: pip failed, webdriver will fail to connect"
+    "${PYTHON}" -m pip install --user \
+        "pytest==4.6.9" \
+        "pytest-ordering==0.6" \
+        "zipp==1.2.0"
+
+    for scenario in "${test_scenarios[@]}"; do
+        if [[ "$scenario" == *pytest* ]]; then
+            echo "Running test scenario ${scenario##*/} with pytest"
+            env_run_pytest "$scenario" || failed=true
+        else
+            echo "Running test scenario ${scenario##*/}"
+            env_run_test "$scenario" || failed=true
+        fi
+
+        if [[ -n "$OST_SKIP_COLLECT" ]]; then
+            if [[ "$failed" == "true" ]]; then
+                env_collect "$PWD/test_logs/${SUITE##*/}/post-${scenario##*/}"
+            fi
+        else
+            env_collect "$PWD/test_logs/${SUITE##*/}/post-${scenario##*/}"
+        fi
+        if $failed; then
+            echo "@@@@ ERROR: Failed running $scenario"
+            return 1
+        fi
+    done
+
+    generate_vdsm_coverage_report
+}

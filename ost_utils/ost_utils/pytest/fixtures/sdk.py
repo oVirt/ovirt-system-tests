cdaa5eaf1ee455d4f12442b0dbcd5233de6cd119
Marcin Sobczyk <msobczyk@redhat.com>
2020-09-30 12:41:17 +0200

    ost_utils: Introduce 'vm' and 'sdk' fixture modules
    This patch introduces two new modules with pytest fixtures -
    'vm' and 'sdk'. The fixtures provide useful tools that help
    accessing SDK entities and querying information on VMs.
    All the implementations are lago-free and are meant to replace
    their lago-based equivalents in subsequent patches.
    
    Change-Id: Ibfe907c432aaf3a5c61f64608ede11a584b95b12
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

---
+++ ost_utils/ost_utils/pytest/fixtures/sdk.py
@@ -0,0 +1,68 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import pytest
+
+
+from ost_utils.pytest.fixtures.engine import *
+
+
+@pytest.fixture(scope="session")
+def system_service(engine_api):
+    return engine_api.system_service()
+
+
+@pytest.fixture(scope="session")
+def hosts_service(system_service):
+    return system_service.hosts_service()
+
+
+@pytest.fixture(scope="session")
+def disks_service(system_service):
+    return system_service.disks_service()
+
+
+@pytest.fixture(scope="session")
+def events_service(system_service):
+    return system_service.events_service()
+
+
+@pytest.fixture(scope="session")
+def storage_domains_service(system_service):
+    return system_service.storage_domains_service()
+
+
+@pytest.fixture(scope="session")
+def vms_service(system_service):
+    return system_service.vms_service()
+
+
+@pytest.fixture(scope="session")
+def get_vm_service_for_vm(vms_service):
+
+    def service_for(vm_name):
+        vms = vms_service.list(search='name={}'.format(vm_name))
+        if len(vms) != 1:
+            raise RuntimeError("Could not find vm: {}".format(vm_name))
+        return vms_service.vm_service(vms[0].id)
+
+    return service_for
---
+++ ost_utils/ost_utils/pytest/fixtures/vm.py
@@ -0,0 +1,69 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+from __future__ import absolute_import
+
+import netaddr.core
+import netaddr.ip
+
+import pytest
+
+from ost_utils.pytest.fixtures.ansible import *
+from ost_utils.pytest.fixtures.sdk import *
+
+
+@pytest.fixture(scope="session")
+def get_hosts_hostname_for_vm(hosts_service, get_vm_service_for_vm):
+
+    def hostname_for(vm_name):
+        vm_service = get_vm_service_for_vm(vm_name)
+        host_id = vm_service.get().host.id
+        hosts = hosts_service.list(search='id={}'.format(host_id))
+        if len(hosts) != 1:
+            raise RuntimeError(
+                "Could not find host for vm: {}".format(vm_name)
+            )
+        return hosts[0].name
+
+    return hostname_for
+
+
+@pytest.fixture(scope="session")
+def get_ansible_host_for_vm(ansible_by_hostname, get_hosts_hostname_for_vm):
+
+    def ansible_host_for(vm_name):
+        return ansible_by_hostname(get_hosts_hostname_for_vm(vm_name))
+
+    return ansible_host_for
+
+
+@pytest.fixture(scope="session")
+def get_vm_ip(get_ansible_host_for_vm):
+
+    def get_ip(vm_name_or_ip):
+        try:
+            netaddr.ip.IPAddress(vm_name_or_ip)
+            return vm_name_or_ip
+        except netaddr.core.AddrFormatError:
+            ansible_host = get_ansible_host_for_vm(vm_name_or_ip)
+            ret = ansible_host.shell('dig +short {}'.format(vm_name_or_ip))
+            return ret["stdout"].strip()
+
+    return get_ip

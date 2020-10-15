520279523b059ac914826938d066bb727acb3017
Marcin Sobczyk <msobczyk@redhat.com>
2020-10-15 16:12:05 +0200

    setup: Add setup playbook and script
    This patch adds an ansible playbook and a convenience script
    for properly setting up a bare system to run OST.
    
    The playbook is actually only a slightly adapted version of Milan's
    work shared offline. Kudos Milan!
    
    Usage:
    
     ./setup_for_ost.sh
    
    For automated scenarios there's --assume-yes option (please note that it
    will work only if you have passwordless sudo - otherwise you'll still need
    to provide the sudo password interactively):
    
     ./setup_for_ost.sh -y
    
    some variables from the playbook can be overriden with a YAML/JSON
    file (see the playbook and ansible for details):
    
     ./setup_for_ost.sh myvars.json
    
    where:
    
     $ cat myvars.json
    
     {
         "ost_images_repo_url": "http://other.repo.org/",
         "ost_images_rpms": [
             "ost-images-rhel8-engine-installed",
             "ost-images-rhel8-host-installed"
         ]
     }
    
    The only supported distros are el8/rhel8, which is dictated mainly
    by lago.
    
    Change-Id: I75870f7e5fc0b4e7471e1deb0544296a5fec4981
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

---
+++ setup_for_ost.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: $0 [-h|--help] [-y|--assume-yes] [ANSIBLE_EXTRA_VARS_FILE]"
+    echo
+	echo "For automated scenarios there's --assume-yes option (please note that it"
+	echo "will work only if you have passwordless sudo - otherwise you'll still need"
+    echo "to provide the sudo password interactively):"
+	echo
+	echo " ./setup_for_ost.sh -y"
+	echo
+	echo "some variables from the playbook can be overriden with a YAML/JSON"
+	echo "file (see the playbook and ansible for details):"
+	echo
+	echo " ./setup_for_ost.sh myvars.json"
+	echo
+	echo "where:"
+	echo
+	echo " $ cat myvars.json"
+	echo
+	echo " {"
+	echo '     "ost_images_repo_url": "http://other.repo.org/",'
+	echo '     "ost_images_rpms": ['
+	echo '         "ost-images-rhel8-engine-installed",'
+	echo '         "ost-images-rhel8-host-installed"'
+	echo "     ]"
+	echo " }"
+}
+
+while [[ "${#}" -gt 0 ]]; do
+    case ${1} in
+        -y|--assume-yes)
+            ASSUME_YES=1
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        *)
+            ANSIBLE_EXTRA_VARS_FILE="${1}"
+            ;;
+    esac
+    shift
+done
+
+if [[ ${EUID} -eq 0 ]]; then
+    echo "This script should be run as a non-root user with sudo access."
+    exit 1
+fi
+
+ANSIBLE_INSTALLED=$(which ansible-playbook &> /dev/null && echo 1 || echo 0)
+ANSIBLE_REPO_RPM_URL=${ANSIBLE_REPO_RPM_URL:-"https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"}
+ANSIBLE_REPO_RPM_NAME=${ANSIBLE_REPO_RPM_NAME:-"epel-release-8"}
+
+cleanup() {
+    if [[ ${REMOVE_ANSIBLE_REPO_RPM} -eq 1 ]]; then
+        sudo dnf remove -y "${ANSIBLE_REPO_RPM_NAME}"
+    fi
+}
+
+trap cleanup EXIT
+
+if [[ ${ASSUME_YES} -ne 1 ]]; then
+    echo "You're running this setup as \"$(whoami)\" user"
+    echo "That means you will have to run OST (or use lago in general) as the same user."
+    echo "You will still be asked for sudo password during setup to run some commands as root."
+    echo
+    echo "Please note, that running OST requires ~6.5GBs of space on the root partition constantly"
+    echo "and around twice as much during 'dnf update'. If that's a problem, one way to work around"
+    echo "is to mount/symlink '/usr/share/ost-images' directory to something more capacious."
+    echo
+    read -p "Type 'y' if you want to continue " -n 1 -r
+    echo
+    if [[ ! ${REPLY} =~ ^[Yy]$ ]]; then
+        exit 0
+    fi
+fi
+
+if [[ ${ANSIBLE_INSTALLED} -eq 0 ]]; then
+    echo "This script needs ansible to work properly, will install it now..."
+    sudo dnf install -y ansible
+    if [[ ${?} -ne 0 ]]; then
+        REMOVE_ANSIBLE_REPO_RPM=1
+        sudo dnf -y install "${ANSIBLE_REPO_RPM_URL}" && sudo dnf install -y ansible
+    fi
+fi
+
+echo "Running the setup playbook..."
+
+if [[ ${ASSUME_YES} -eq 1 ]]; then
+    ANSIBLE_ASK_SUDO_PASS_FLAG=""
+else
+    ANSIBLE_ASK_SUDO_PASS_FLAG="-K"
+fi
+
+if [[ -n ${ANSIBLE_EXTRA_VARS_FILE} ]]; then
+    ANSIBLE_EXTRA_VARS_FLAG="-e"
+    ANSIBLE_EXTRA_VARS_FILE="@${ANSIBLE_EXTRA_VARS_FILE}"
+else
+    ANSIBLE_EXTRA_VARS_FLAG=""
+fi
+
+ansible-playbook \
+    --connection=local \
+    -i 127.0.0.1, \
+    ${ANSIBLE_ASK_SUDO_PASS_FLAG} \
+    ${ANSIBLE_EXTRA_VARS_FLAG} ${ANSIBLE_EXTRA_VARS_FILE} \
+    common/setup/setup_playbook.yml

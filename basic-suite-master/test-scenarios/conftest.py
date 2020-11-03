c88010cc0ea6bac412f56f0fb78aa90d97482f97
Marcin Sobczyk <msobczyk@redhat.com>
2020-11-03 13:27:36 +0100

    basic: Report start times and runnings time of tests
    This patch adds 2 pytest hooks that show at what time
    the test has started and how long did it take to complete.
    
    Change-Id: I31d2421bfc1983a0d33e8ef52e74e3ffbf6502a9
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- basic-suite-master/test-scenarios/conftest.py
+++ basic-suite-master/test-scenarios/conftest.py
@@ -25,6 +25,7 @@ from ost_utils.pytest import pytest_collection_modifyitems
 from ost_utils.pytest.fixtures.ansible import ansible_clean_private_dirs
 from ost_utils.pytest.fixtures.ansible import ansible_collect_logs
 from ost_utils.pytest.fixtures.artifacts import artifacts_dir
+from ost_utils.pytest.running_time import *
 
 
 if os.environ.get("USE_LAGO_OST_PLUGIN", "0") == "1":

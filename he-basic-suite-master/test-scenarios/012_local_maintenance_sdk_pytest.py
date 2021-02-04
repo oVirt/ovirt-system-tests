050ce7371fd92cb9ddf6ede9f47ab06bc23f75a7
Yedidyah Bar David <didi@redhat.com>
2021-02-04 12:12:28 +0200

    HE: Enhance 012_local_maintenance_sdk_pytest.py
    - Do all operations within assert_true_within_long
    When I ran this test in a loop, sometimes it failed to activate or
    deacrivate due to what seems like temporary reasons. So try harder.
    
    - Add and improve info texts.
    
    - Remove unneeded 'Waiting For System Stability' in the middle
    
    - Remove check that host is UNASSIGNED
    IMO this is test is not needed, and in principle might fail simply
    because it moved faster than expected from maintenance to active/up.
    
    Change-Id: I59762b7e4d6eea6c3251f87bcb8904d7ec440f60
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- he-basic-suite-master/test-scenarios/012_local_maintenance_sdk_pytest.py
+++ he-basic-suite-master/test-scenarios/012_local_maintenance_sdk_pytest.py
@@ -39,6 +39,20 @@ def setup_module():
     ipv6_utils.open_connection_to_api_with_ipv6_on_relevant_suite()
 
 
+def _hosted_engine_info(hosted_engine):
+    """Input: ovirtsdk4.types.HostedEngine instance
+    Return: dict with info
+    """
+    props = (
+        'score',
+        'configured',
+        'global_maintenance',
+        'active',
+        'local_maintenance',
+    )
+    return {p: getattr(hosted_engine, p) for p in props}
+
+
 def test_local_maintenance(hosts_service, get_vm_service_for_vm):
     logging.info('Waiting For System Stability...')
     # TODO: Replace arbitrary sleep with something more sensible
@@ -48,32 +62,52 @@ def test_local_maintenance(hosts_service, get_vm_service_for_vm):
     he_host_id = vm_service.get().host.id
     host_service = hosts_service.host_service(id=he_host_id)
 
+    def do_verified_deactivation():
+        logging.debug(f'Trying to deactivate host {host_service.get().name}')
+        try:
+            host_service.deactivate()
+        except ovirtsdk4.Error:
+            # Ignore. Just return the result and let the caller fail if needed
+            pass
+        status = host_service.get().status
+        hosted_engine = host_service.get(all_content=True).hosted_engine
+        logging.debug(f'status={status}')
+        logging.debug(f'hosted_engine={_hosted_engine_info(hosted_engine)}')
+        # Original test was:
+        #   (
+        #       status == types.HostStatus.MAINTENANCE or
+        #       hosted_engine.local_maintenance
+        #   )
+        # But this does not test local_maintenance (presumably the "local
+        # maintenance" status as reported by the HA daemons?).
+        # So I tried to change the "or" to "and" (require both), and it
+        # never happened - local_maintenance always remained False.
+        # Giving up on this for now and checking only status.
+        # TODO: Find out why, fix what's needed, change the code to require
+        # both. Also for do_verified_activation below.
+        return status == types.HostStatus.MAINTENANCE
+
     logging.info('Performing Deactivation...')
-    host_service.deactivate()
-    assertions.assert_true_within_long(
-        lambda: (
-            host_service.get().status == types.HostStatus.MAINTENANCE or
-            host_service.get(all_content=True).hosted_engine.local_maintenance
-        ),
-        allowed_exceptions=[ovirtsdk4.Error],
-    )
+    assertions.assert_true_within_long(do_verified_deactivation)
+
+    def do_verified_activation():
+        logging.info(f'Trying to activate host {host_service.get().name}')
+        try:
+            host_service.activate()
+        except ovirtsdk4.Error:
+            # Ignore. Just return the result and let the caller fail if needed
+            pass
+        status = host_service.get().status
+        hosted_engine = host_service.get(all_content=True).hosted_engine
+        logging.debug(f'status={status}')
+        logging.debug(f'hosted_engine={_hosted_engine_info(hosted_engine)}')
+        # TODO See comment above
+        return status == types.HostStatus.UP
 
     logging.info('Performing Activation...')
-    host_service.activate()
-    assertions.assert_true_within_long(
-        lambda: host_service.get().status == types.HostStatus.UNASSIGNED
-    )
+    assertions.assert_true_within_long(do_verified_activation)
 
-    logging.info('Waiting For System Stability...')
-    # TODO: Replace arbitrary sleep with something more sensible
-    time.sleep(WAIT_VALUE)
-
-    logging.info('Waiting For Maintenance...')
-    assertions.assert_true_within_long(
-        lambda: not host_service.get(all_content=True).hosted_engine.local_maintenance
-    )
-
-    logging.info('Waiting For Score...')
+    logging.info('Verifying that all hosts have score higher than 0...')
     assertions.assert_true_within_long(
         lambda: host_service.get(all_content=True).hosted_engine.score > 0
     )

32d42dd846feaf4f1bc6f50227160d426b7bab41
Lucia Jelinkova <ljelinko@redhat.com>
2021-02-25 13:43:33 +0100

    UI tests: Change Grafana login
    The Grafana login page changed between version,
    this fix should allow to use both of them interchangeably.
    
    Change-Id: I01664d24c541847e3f203aaa23fe8db537d10997
    Signed-off-by: Lucia Jelinkova <ljelinko@redhat.com>

--- basic-suite-master/test-scenarios/test_100_basic_ui_sanity.py
+++ basic-suite-master/test-scenarios/test_100_basic_ui_sanity.py
@@ -535,7 +535,6 @@ def test_userportal(ovirt_driver, save_screenshot, save_page_source,
         raise
 
 
-@pytest.mark.xfail(reason="fails on Stream because of different grafana UI")
 def test_grafana(ovirt_driver, save_screenshot, save_page_source, engine_username,
                engine_password, engine_webadmin_url):
     try:
--- ost_utils/ost_utils/selenium/page_objects/GrafanaLoginScreen.py
+++ ost_utils/ost_utils/selenium/page_objects/GrafanaLoginScreen.py
@@ -2,16 +2,18 @@ from .Displayable import Displayable
 
 class GrafanaLoginScreen(Displayable):
 
+    OAUTH_XPATH = '//*[@href="login/generic_oauth"]'
+
     def __init__(self, ovirt_driver):
         super(GrafanaLoginScreen, self).__init__(ovirt_driver)
 
     def is_displayed(self):
-        return self.ovirt_driver.driver.find_element_by_class_name('login-oauth').is_displayed()
+        return self.ovirt_driver.driver.find_element_by_xpath(self.OAUTH_XPATH).is_displayed()
 
     def get_displayable_name(self):
         return 'Grafana login screen'
 
     def use_ovirt_engine_auth(self):
         print ('Open oVirt Engine Auth')
-        self.ovirt_driver.driver.find_element_by_class_name('login-oauth').click()
+        self.ovirt_driver.xpath_click(self.OAUTH_XPATH)
 

40687b1f9ae9ad6ec91d918d688819884960c945
Yedidyah Bar David <didi@redhat.com>
2021-03-10 15:46:54 +0200

    Log tests to pytest log
    Change-Id: Iffce5c2717e0856ba8df6ce12a5e3af8df66d5b3
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- ost_utils/ost_utils/pytest/running_time.py
+++ ost_utils/ost_utils/pytest/running_time.py
@@ -20,6 +20,9 @@
 
 import datetime
 
+import logging
+LOGGER = logging.getLogger('')
+
 
 RUNNING_TIMES = {}
 
@@ -28,6 +31,7 @@ def pytest_runtest_logstart(nodeid, location):
     now = datetime.datetime.now()
     RUNNING_TIMES[location] = now
     print(now.strftime('started at %Y-%m-%d %H:%M:%S'), end=' ')
+    LOGGER.debug(f'Running test: {nodeid}')
 
 
 def pytest_runtest_logfinish(nodeid, location):
@@ -35,3 +39,4 @@ def pytest_runtest_logfinish(nodeid, location):
     then = RUNNING_TIMES[location]
     delta = int((now - then).total_seconds())
     print(" ({}s)".format(delta), end='')
+    LOGGER.debug(f'Finished test: {nodeid} ({delta}s)')

fd71a8ae5cacf874a7ed0a9e1e71e44adcd3799c
Marcin Sobczyk <msobczyk@redhat.com>
2021-04-15 10:42:33 +0200

    init: Cleanup lago init files
    None of the removed entries are in effect anymore:
    - we use pub/priv keys for ssh
    - we use ansible for handling systemd units instead of lago
    - we don't need to define the distro (except for HE's engine)
    - the artifact lists already contain removed entries
    - 'snapshot-live-merge' metadata is not used
    - 'setup_1st_host_el7.sh' was used before we had OST images
      to install 'ovirt-host' during OST run, but we no longer do that
    
    Change-Id: I7ebfb83d572f9d069539e453901ab19b22c80feb
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- common/deploy-scripts/setup_1st_host_el7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-set -ex
-
-yum -y install ovirt-host
-rm -rf /var/cache/yum/* /var/cache/dnf/*
-
-virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf domcapabilities kvm > /var/log/virsh_domcapabilities.log || res=$?

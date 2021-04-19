fae4dbed55814035bd35314d5a419c392d713687
Eitan Raviv <eraviv@redhat.com>
2021-04-19 17:51:09 +0300

    network: adaptable machine facts
    ost_utils.ansible.ansible_facts retrieves information from the remote
    machine whenever its get attribute is called. When an ansible service is
    present and fetches the facts MachineFacts caches the ansible response
    as a simple collection of machine access info.
    When an ansible service is not present in an independent environment
    MachineFacts can be easily instantiated with access info for the engine
    and hosts in the environment, enabling fast and easy setup for testing
    and debugging.
    
    MachineFacts therefore serves as an adapter that encapsulates dependency
    upon the ost_utils-ansible stack from the network suite for multiple
    debugging and testing environments.
    
    Change-Id: I22af0e263b4619135c2ecfb858ff6c944aafb891
    Signed-off-by: Eitan Raviv <eraviv@redhat.com>

--- network-suite-master/fixtures/ansible.py
+++ network-suite-master/fixtures/ansible.py
@@ -26,17 +26,26 @@ from ost_utils.ansible import private_dir
 
 @pytest.fixture(scope="session")
 def engine_facts():
-    return AnsibleFactsCache(facts.engine())
+    return MachineFacts(
+        facts.engine().get('ansible_default_ipv4').get('address'),
+        facts.engine().get('ansible_hostname')
+    )
 
 
 @pytest.fixture(scope="session")
 def host0_facts():
-    return AnsibleFactsCache(facts.host0())
+    return MachineFacts(
+        facts.host0().get('ansible_default_ipv4').get('address'),
+        facts.host0().get('ansible_hostname')
+    )
 
 
 @pytest.fixture(scope="session")
 def host1_facts():
-    return AnsibleFactsCache(facts.host1())
+    return MachineFacts(
+        facts.host1().get('ansible_default_ipv4').get('address'),
+        facts.host1().get('ansible_hostname')
+    )
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -51,23 +60,30 @@ def ansible_collect_logs(artifacts_dir, ansible_clean_private_dirs):
     ansible.LogsCollector.save(artifacts_dir)
 
 
-class AnsibleFactsCache(object):
-
+class MachineFacts(object):
     """
-    ost_utils.ansible.ansible_facts retrieves information from
-    the remote machine whenever its get(attribute) is called.
-    AnsibleFactsCache caches the remote responses while also serving as
-    an adapter that encapsulates the ost_utils.
-    ansible library from the network suite.
+    ost_utils.ansible.ansible_facts retrieves information from the remote
+    machine whenever its get attribute is called. When an ansible service is
+    present and fetches the facts MachineFacts caches the ansible response as a
+    simple collection of machine access info.
+    When an ansible service is not present in an independent environment
+    MachineFacts can be easily instantiated with access info for the engine and
+    hosts in the environment, enabling fast and easy setup for testing and
+    debugging.
+
+    MachineFacts therefore serves as an adapter that encapsulates dependency
+    upon the ost_utils-ansible stack from the network suite for multiple use
+    cases.
     """
 
-    def __init__(self, ansible_facts):
-        self._ipv4_default = ansible_facts.get('ansible_default_ipv4')
-        self._hostname = ansible_facts.get('ansible_hostname')
+    def __init__(self, ipv4_default, hostname, ssh_password='123456'):
+        self._ipv4_default = ipv4_default
+        self._hostname = hostname
+        self._ssh_password = ssh_password
 
     @property
     def ipv4_default_address(self):
-        return self._ipv4_default.get('address')
+        return self._ipv4_default
 
     @property
     def hostname(self):
@@ -75,4 +91,4 @@ class AnsibleFactsCache(object):
 
     @property
     def ssh_password(self):
-        return '123456'
+        return self._ssh_password

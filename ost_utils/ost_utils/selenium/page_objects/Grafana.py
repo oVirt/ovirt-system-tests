2c69bec6a457ca8c9a76ad30fe14846acfd3a8aa
Lucia Jelinkova <ljelinko@redhat.com>
2021-06-01 14:24:05 +0200

    UI tests: Update Grafana page object
    Grafana UI changed, updated page object to support
    both, old and new UI.
    
    Change-Id: I4aad8a89743af82d215f5bb38ed74bf3a8cb2460
    Signed-off-by: Lucia Jelinkova <ljelinko@redhat.com>

--- ost_utils/ost_utils/selenium/page_objects/Grafana.py
+++ ost_utils/ost_utils/selenium/page_objects/Grafana.py
@@ -19,9 +19,9 @@ class Grafana(Displayable):
     def open_dashboard(self, menu, submenu):
         LOGGER.debug('Open dashboard ' + menu + '/' + submenu)
         self.ovirt_driver.xpath_wait_and_click('Grafana logo button', '//*[@class="sidemenu__logo"]')
-        self.ovirt_driver.xpath_wait_and_click('Home button', '//div[@class="navbar"]//a[normalize-space()="Home"]')
-        self.ovirt_driver.xpath_wait_and_click(menu, '//*[text() = "' + menu + '"]')
-        self.ovirt_driver.xpath_wait_and_click(submenu, '//*[text() = "' + submenu + '"]')
+        self.ovirt_driver.xpath_wait_and_click('Home button', '//div[@class="navbar"]//a[normalize-space()="Home"] | //button[normalize-space()="Home"]')
+        self.ovirt_driver.xpath_wait_and_click(menu, f'//*[text() = "{menu}"]')
+        self.ovirt_driver.xpath_wait_and_click(submenu, f'//*[text() = "{submenu}"]')
 
         self.ovirt_driver.wait_until('Breadcrumbs visible', self._is_breadcrumbs_visible, menu, submenu)
 
@@ -29,6 +29,6 @@ class Grafana(Displayable):
         return self.ovirt_driver.is_class_name_present('alert-error') and self.ovirt_driver.driver.find_element_by_class_name('alert-error').is_displayed()
 
     def _is_breadcrumbs_visible(self, menu, submenu):
-        is_breadcrumb_menu_visible = self.ovirt_driver.driver.find_element_by_xpath('//div[@class="navbar-page-btn"]//a[text() = "' + menu + '"]')
-        is_breadcrumb_submenu_visible = self.ovirt_driver.driver.find_element_by_xpath('//div[@class="navbar-page-btn"]//a[text() = "' + submenu + '"]')
+        is_breadcrumb_menu_visible = self.ovirt_driver.driver.find_element_by_xpath(f'//div[@class="navbar-page-btn"]//a[text() = "{menu}"] | //button[text() = "{menu}"]')
+        is_breadcrumb_submenu_visible = self.ovirt_driver.driver.find_element_by_xpath(f'//div[@class="navbar-page-btn"]//a[text() = "{submenu}"] | //button[text() = "{submenu}"]')
         return is_breadcrumb_menu_visible and is_breadcrumb_submenu_visible

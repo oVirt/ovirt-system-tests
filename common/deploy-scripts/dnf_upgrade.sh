26aa87d98f32bbe1b69cfb946f2585dc64e6804e
Michal Skrivanek <michal.skrivanek@redhat.com>
2021-06-03 15:07:27 +0200

    deploy: clean up add_plain_repos/dnf upgrade
    this is obsolete since we switched to "non-lago deployment"
    
    Change-Id: I72ab9f72b7e25b148cfdb1a8b1b5046e0e918b99
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- common/deploy-scripts/dnf_upgrade.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -xe
-
-dnf upgrade -y --nobest --nogpgcheck
--- ost_utils/ost_utils/backend/base.py
+++ ost_utils/ost_utils/backend/base.py
@@ -102,17 +102,14 @@ class BaseBackend(abc.ABC):
 
             {
                 'lago-basic-suite-master-engine': [
-                    '$OST_REPO_ROOT/common/deploy-scripts/dnf_upgrade.sh',
                     '$OST_REPO_ROOT/common/deploy-scripts/setup_sar_stat.sh',
                     '$OST_REPO_ROOT/common/deploy-scripts/setup_engine.sh',
                 ],
                 'lago-basic-suite-master-host-0': [
-                    '$OST_REPO_ROOT/common/deploy-scripts/dnf_upgrade.sh',
                     '$OST_REPO_ROOT/common/deploy-scripts/setup_sar_stat.sh',
                     '$OST_REPO_ROOT/common/deploy-scripts/setup_host.sh',
                 ],
                 'lago-basic-suite-master-host-1': [
-                    '$OST_REPO_ROOT/common/deploy-scripts/dnf_upgrade.sh',
                     '$OST_REPO_ROOT/common/deploy-scripts/setup_sar_stat.sh',
                     '$OST_REPO_ROOT/common/deploy-scripts/setup_host.sh',
                 ]

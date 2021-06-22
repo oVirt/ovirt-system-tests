dd5543c9dcab4647344c1793cad2db16ebcbd6ef
Marcin Sobczyk <msobczyk@redhat.com>
2021-06-22 09:53:23 +0200

    he: Don't add additional '/var/tmp'
    it broke after cf98ba8037d4e8ddfbf411a5e38553c62d46c5fa since we use
    /var/tmp for dummy repo. There is no need for extra space, it currently
    fits the deployment and leaves ~450MB free on / and we'll bump the base
    image size by 1GB in https://gerrit.ovirt.org/#/c/ost-images/+/115355
    
    Change-Id: I99affb2c32f04ebf646bcdda5d050d12803b3ec3
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- common/deploy-scripts/add_tmp_part.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TMP_DEV="/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_2"
-
-mkfs.xfs -K ${TMP_DEV}
-mount -t xfs ${TMP_DEV} /var/tmp
-chmod 1777 /var/tmp
-echo -e "${TMP_DEV} /var/tmp xfs defaults 0 0" >> /etc/fstab

3c816c203c6a81e34660640b5f7f4a00a26fe38e
Yedidyah Bar David <didi@redhat.com>
2021-07-27 09:26:51 +0300

    HE: dig loop: Enhance log
    Make each line include also the result for this run, and not only the
    totals.
    
    Change-Id: I9f251d1cb1bea1ed762851d781ed21ba4fe72e5f
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- he-basic-suite-master/dig_loop/run_dig_loop.sh
+++ he-basic-suite-master/dig_loop/run_dig_loop.sh
@@ -6,9 +6,13 @@ total=0
 good=0
 while true; do
     total=$(expr ${total} + 1)
+    res='Failed'
     dig +tries=1 +time=1 >/dev/null 2>&1 &
     pid=$!
     sleep 2
-    wait "${pid}" && good=$(expr ${good} + 1)
-    echo "$(date) dig loop: Passed ${good} out of ${total}" >> "${LOG}"
+    if wait "${pid}"; then
+        res='Succeeded'
+        good=$(expr ${good} + 1)
+    fi
+    echo "$(date) dig ${res}: Passed ${good} out of ${total}" >> "${LOG}"
 done

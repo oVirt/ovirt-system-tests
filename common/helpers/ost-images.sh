242d10121e7af2589028bf8f32ee8cc7bb141cb1
Milan Zamazal <mzamazal@redhat.com>
2021-07-28 14:38:27 +0200

    ost-images: Simplify using OST images without their rpm's
    The rpm packages may not be installed but the images can still be
    present.  For instance when ost-images directory is mounted to a
    container, without the need to fetch the large packages into the
    container or to reuse them in multiple containers.
    
    While it is already possible to specify individual locations of images
    and keys via numerous OST_IMAGES_* variables, it is cumbersome for
    the purpose of using standard images and breaks in case anything in
    that set of variables changes.  Let's introduce OST_IMAGES_DIR
    variable specifying the directory containing OST images.
    
    Change-Id: Ie717b7a26e2dde3facb43cb72b05aa8337ece1be
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>

--- common/helpers/ost-images.sh
+++ common/helpers/ost-images.sh
@@ -1,15 +1,24 @@
 #!/bin/bash -xe
 
 _find_qcow() {
-    rpm -q ost-images-${OST_IMAGES_DISTRO}-${1} 1>&2
-    rpm -ql ost-images-${OST_IMAGES_DISTRO}-${1} | grep qcow2
+    if [ -n "$OST_IMAGES_DIR" ]; then
+        ls "${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-${1}.qcow2" 2>/dev/null
+    else
+        rpm -q ost-images-${OST_IMAGES_DISTRO}-${1} 1>&2
+        rpm -ql ost-images-${OST_IMAGES_DISTRO}-${1} | grep qcow2
+    fi
 }
 
 _find_ssh_key() {
-    rpm -ql ost-images-${OST_IMAGES_DISTRO}-base | grep 'id_rsa$'
+    if [ -n "$OST_IMAGES_DIR" ]; then
+        ls "${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}_id_rsa" 2>/dev/null
+    else
+        rpm -ql ost-images-${OST_IMAGES_DISTRO}-base | grep 'id_rsa$'
+    fi
 }
 
 export OST_IMAGES_DISTRO=${OST_IMAGES_DISTRO:-el8stream}
+[ -n "$OST_IMAGES_DIR" ] && echo "Using ${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-* for ost-images"
 
 export OST_IMAGES_BASE=${OST_IMAGES_BASE:-$(_find_qcow "base")}
 export OST_IMAGES_NODE=${OST_IMAGES_NODE:-$(OST_IMAGES_DISTRO=node _find_qcow "base")}

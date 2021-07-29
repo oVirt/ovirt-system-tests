b3aadb7909fc71a216b45fb3378b2e86edf9388e
Pavel Bar <pbar@redhat.com>
2021-07-29 14:33:50 +0300

    backup: temporary fix OST backup test due to race conditions
    Due to race conditions as described in bugs 1923178 & 1984308
    multiple OST runs fail.
    This fix adds a temporary workaround to run the failing
    incremental backup up to 3 times with a 10-seconds sleep
    in case that those specific failures are encountered.
    That should stabilize the OST for now.
    
    Signed-off-by: Pavel Bar <pbar@redhat.com>
    Change-Id: Ibd0ac89591eb99212b10a6082ab24f49a5fdcb7b

--- ost_utils/ost_utils/storage_utils/backup.py
+++ ost_utils/ost_utils/storage_utils/backup.py
@@ -18,10 +18,15 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
+from time import sleep
+import logging
+
 import ovirtsdk4 as sdk4
 import ovirtsdk4.types as types
 
 from ost_utils import assertions
+
+LOGGER = logging.getLogger(__name__)
 
 
 def perform_vm_backup(
@@ -67,12 +72,29 @@ def perform_incremental_vm_backup(engine_api, backups_service, disk_name, correl
         disks_service, disk,
         correlation_id="full_" + correlation_id)
 
-    # Start an incremental backup using the checkpoint created before
-    perform_vm_backup(
-        backups_service,
-        disks_service, disk,
-        from_checkpoint_id=full_checkpoint_id,
-        correlation_id="incremental_" + correlation_id)
+    # TODO: remove when bug #1923178 is fixed.
+    # Protecting from the following errors:
+    # 16:43:32 E ovirtsdk4.Error: Fault reason is "Operation Failed". Fault detail is "[Cannot backup VM: Disk is locked. Please try again later.]". HTTP response code is 409.
+    # 14:17:18 E ovirtsdk4.Error: Fault reason is "Operation Failed". Fault detail is "[Cannot backup VM. The VM is during a backup operation.]". HTTP response code is 409.
+    allowed_errors = ["Fault reason is \"Operation Failed\". Fault detail is \"[Cannot backup VM: Disk is locked. Please try again later.]\". HTTP response code is 409.",
+                      "Fault reason is \"Operation Failed\". Fault detail is \"[Cannot backup VM. The VM is during a backup operation.]\". HTTP response code is 409."]
+    # 3 retries are allowed until failing.
+    for i in range(3):
+        try:
+            # Start an incremental backup using the checkpoint created before
+            perform_vm_backup(
+                backups_service,
+                disks_service, disk,
+                from_checkpoint_id=full_checkpoint_id,
+                correlation_id="incremental_" + correlation_id)
+        except sdk4.Error as err:
+            LOGGER.warning("Exception: {} ".format(err))
+            if i < 2 and err.args[0] in allowed_errors:
+                LOGGER.info("Incremental backup failed due to race condition, "
+                            "sleep 10 seconds and try again up to 3 times.")
+                sleep(10)
+                continue
+            raise
 
 
 def remove_vm_root_checkpoint(checkpoints_service):

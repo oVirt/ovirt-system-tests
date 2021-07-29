60f140bb243394f3ca5d86ff7b6eac1fe4601e58
Yedidyah Bar David <didi@redhat.com>
2021-07-29 15:42:38 +0300

    HE: run_dig_loop: Run forever, until stopped
    Change-Id: Ic2c25e3c8d0f09b72ed752b6565b8a57c2a789ba
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- he-basic-suite-master/dig_loop/run_dig_loop.sh
+++ he-basic-suite-master/dig_loop/run_dig_loop.sh
@@ -2,12 +2,9 @@
 
 LOG="/var/log/run_dig_loop.log"
 
-starttime=$(date +%s)
-endtime=$(expr "${starttime}" + 7200)
-
 total=0
 good=0
-while [ $(date +%s) -lt "${endtime}" ]; do
+while true; do
     total=$(expr ${total} + 1)
     dig +tries=1 +time=1 >/dev/null 2>&1 &
     pid=$!

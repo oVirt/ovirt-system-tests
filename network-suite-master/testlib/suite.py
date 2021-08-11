3255b6623e857675816e5d0a6c1180fe0d10ec8b
Eitan Raviv <eraviv@redhat.com>
2021-08-11 20:51:18 +0300

    network: temp variable name for ip version
    Until infrastructure support ipv4 only and ipv6
    
    Change-Id: Ifa00ef5633f615d9933e96b0c3e7cb6b47401d66
    Signed-off-by: Eitan Raviv <eraviv@redhat.com>

--- network-suite-master/testlib/suite.py
+++ network-suite-master/testlib/suite.py
@@ -43,7 +43,7 @@ def af():
     IP_VERSION environment variable expected values: '4' or '6'.
     """
     try:
-        return address_family.AF(os.environ['IP_VERSION'])
+        return address_family.AF(os.environ['IP_VERSION_TEMP'])
     except KeyError:
         return address_family.AF('4')
 

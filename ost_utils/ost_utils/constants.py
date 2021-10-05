6da93e4b1e5af4431a4cb0de1a4279055a4839ba
Yedidyah Bar David <didi@redhat.com>
2021-10-05 09:30:07 +0300

    Allow 30 minutes for adding hosts
    20 minutes are not enough in certain cases.
    
    Change-Id: Iec6677abcc313216f29425824b63220583832337
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- ost_utils/ost_utils/constants.py
+++ ost_utils/ost_utils/constants.py
@@ -5,6 +5,10 @@
 #
 
 DEFAULT_OVN_PROVIDER_NAME = 'ovirt-provider-ovn'
-ADD_HOST_TIMEOUT = 20 * 60
+# Used to be 20 minutes, but wasn't enough in certain conditions - basically,
+# the time installation took + 600 seconds hard-coded delay by the engine
+# waiting for the host to reboot. TODO: Make the engine poll the host in a
+# loop instead of waiting a constant time and revert here to 20 minutes.
+ADD_HOST_TIMEOUT = 30 * 60
 ENGINE_VM_RESTART_TIMEOUT = 20 * 60
 FLOATING_DISK_NAME = 'floating_disk'

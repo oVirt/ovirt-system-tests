1ceb2d6f0253e41e006bd174cdd99dd0215cebaa
Michal Skrivanek <michal.skrivanek@redhat.com>
2021-10-25 13:36:49 +0200

    common: do not query rpm for ost-images
    ost-images rpms are installed to /usr/share/ost-images and this is the
    only location we want to use by default. A host can have multiple versions
    of rpms installed - each relocated to a different directory.
    
    Change-Id: I2c359276ffaba404302b2260ab04bd94607397ab
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- common/helpers/ost-images.sh
+++ common/helpers/ost-images.sh
@@ -1,29 +1,13 @@
 #!/bin/bash -xe
 
-_find_qcow() {
-    if [ -n "$OST_IMAGES_DIR" ]; then
-        ls "${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-${1}.qcow2" 2>/dev/null
-    else
-        rpm -q ost-images-${OST_IMAGES_DISTRO}-${1} 1>&2
-        rpm -ql ost-images-${OST_IMAGES_DISTRO}-${1} | grep qcow2
-    fi
-}
+OST_IMAGES_DIR=${OST_IMAGES_DIR:=/usr/share/ost-images}
+OST_IMAGES_DISTRO=${OST_IMAGES_DISTRO:=el8stream}
 
-_find_ssh_key() {
-    if [ -n "$OST_IMAGES_DIR" ]; then
-        ls "${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}_id_rsa" 2>/dev/null
-    else
-        rpm -ql ost-images-${OST_IMAGES_DISTRO}-base | grep 'id_rsa$'
-    fi
-}
+echo ${OST_IMAGES_BASE:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-base.qcow2}
+echo ${OST_IMAGES_NODE:=${OST_IMAGES_DIR}/node-base.qcow2}
+echo ${OST_IMAGES_ENGINE_INSTALLED:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-engine-installed.qcow2}
+echo ${OST_IMAGES_HOST_INSTALLED:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-host-installed.qcow2}
+echo ${OST_IMAGES_HE_INSTALLED:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-he-installed.qcow2}
+[[ -r "${OST_IMAGES_SSH_KEY:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}_id_rsa}" ]] || echo "ssh key file missing"
 
-export OST_IMAGES_DISTRO=${OST_IMAGES_DISTRO:-el8stream}
-[ -n "$OST_IMAGES_DIR" ] && echo "Using ${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-* for ost-images"
-
-export OST_IMAGES_BASE=${OST_IMAGES_BASE:-$(_find_qcow "base")}
-export OST_IMAGES_NODE=${OST_IMAGES_NODE:-$(OST_IMAGES_DISTRO=node _find_qcow "base")}
-export OST_IMAGES_ENGINE_INSTALLED=${OST_IMAGES_ENGINE_INSTALLED:-$(_find_qcow "engine-installed")}
-export OST_IMAGES_HOST_INSTALLED=${OST_IMAGES_HOST_INSTALLED:-$(_find_qcow "host-installed")}
-export OST_IMAGES_HE_INSTALLED=${OST_IMAGES_HE_INSTALLED:-$(_find_qcow "he-installed")}
-
-export OST_IMAGES_SSH_KEY=${OST_IMAGES_SSH_KEY:-$(_find_ssh_key)}
+export $(set | grep ^OST_IMAGES_ | cut -d= -f1)

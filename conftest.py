7c27ef91b3851c7b846648e5443019db563436c4
Andrej Cernek <acernek@redhat.com>
2021-11-02 08:23:13 +0100

    ost_utils: Improve imported asserting
    Pytest by default improves assert messages, but only in test modules.
    This patch enforces the improvement in ost_utils as well.
    
    Change-Id: I5faba8bcabaae1d42d9d3e09d3405f5c3215666b
    Signed-off-by: Andrej Cernek <acernek@redhat.com>

--- conftest.py
+++ conftest.py
@@ -2,4 +2,7 @@
 # Copyright oVirt Authors
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
+import pytest
+pytest.register_assert_rewrite('ost_utils')
+
 from ost_utils.pytest import pytest_addoption

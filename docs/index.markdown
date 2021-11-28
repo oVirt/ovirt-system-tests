99566eedf5a42a24bca9fcf464bfe42f673811ab
Galit <grosenth@redhat.com>
2021-11-28 09:40:06 +0200

    docs: fix oVirt system tests readthedocs
    Change-Id: Iac50a07a8d63a4313d451c2e0853e4a211bcee1a
    Signed-off-by: Galit <grosenth@redhat.com>

---
+++ docs/index.markdown
@@ -0,0 +1,7 @@
+Welcome to oVirt System Tests Documentation Page!
+=================================================
+
+## General Info
+* [OST]('../README.md')
+* [FAQ]('../FAQ.md')
+
--- lagofy.sh
+++ lagofy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!bin/bash
 
 _deployment_exists() {
     [[ -d "$PREFIX" ]] || { echo "no deployment"; return 1; }
@@ -349,6 +349,7 @@ ost_check_dependencies() {
 ost_linters() {
     echo "Running linters..."
     [[ -n "$OST_INITIALIZED" ]] || ost_check_dependencies || return $?
+    ${PYTHON} -m tox -e docs &>> "${OST_REPO_ROOT}/exported-artifacts/tox-deps.log"
     ${PYTHON} -m tox -q -e flake8,pylint,black,broken-symlinks,copyright-notices
 }
 

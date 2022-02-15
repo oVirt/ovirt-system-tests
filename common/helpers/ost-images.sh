e200feccc5c4e170faec5ad2816769c126fd76c3
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-02-15 16:13:55 +0100

    ost-images: fall back to /usr/share/ost-images when using incomplete OST_IMAGES_DISTRO override
    We support OST_IMAGES_DISTRO for custom directory with ost-images. Let's
    have a fallback to system-installed ost-images in case the directory is
    not complete (doesn't provide all the images that the suite needs)

--- common/helpers/ost-images.sh
+++ common/helpers/ost-images.sh
@@ -5,11 +5,15 @@ OST_IMAGES_DISTRO=${OST_IMAGES_DISTRO:=el8stream}
 local node=node-base.qcow2
 [[ "$OST_IMAGES_DISTRO" == "rhel8" ]] && node=rhvh-base.qcow2
 
-echo ${OST_IMAGES_BASE:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-base.qcow2}
-echo ${OST_IMAGES_NODE:=${OST_IMAGES_DIR}/${node}}
-echo ${OST_IMAGES_ENGINE_INSTALLED:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-engine-installed.qcow2}
-echo ${OST_IMAGES_HOST_INSTALLED:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-host-installed.qcow2}
-echo ${OST_IMAGES_HE_INSTALLED:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-he-installed.qcow2}
-[[ -r "${OST_IMAGES_SSH_KEY:=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}_id_rsa}" ]] || echo "ssh key file missing"
+OST_IMAGES_BASE=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-base.qcow2
+OST_IMAGES_NODE=${OST_IMAGES_DIR}/${node}
+OST_IMAGES_ENGINE_INSTALLED=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-engine-installed.qcow2
+OST_IMAGES_HOST_INSTALLED=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-host-installed.qcow2
+OST_IMAGES_HE_INSTALLED=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}-he-installed.qcow2
+OST_IMAGES_SSH_KEY=${OST_IMAGES_DIR}/${OST_IMAGES_DISTRO}_id_rsa
+for i in OST_IMAGES_BASE OST_IMAGES_NODE OST_IMAGES_ENGINE_INSTALLED OST_IMAGES_HOST_INSTALLED OST_IMAGES_HE_INSTALLED OST_IMAGES_SSH_KEY; do
+  [[ -r "${!i}" ]] || declare $i=/usr/share/ost-images/$(basename ${!i})
+  echo "${i} ${!i}"
+done
 
 export $(set | grep ^OST_IMAGES_ | cut -d= -f1)

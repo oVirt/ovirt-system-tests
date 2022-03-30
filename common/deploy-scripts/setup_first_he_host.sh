d22ab342d3d838325ee6f799078bad8c7d4b0573
michalskrivanek <smrkev@gmail.com>
2022-03-30 11:02:30 +0200

    Revert "Use nobest in 'DNF update the system' (#85)"
    This reverts commit 7eb19a6d9b7628ba35fc754d2136476f8c3c4265.

--- common/deploy-scripts/setup_first_he_host.sh
+++ common/deploy-scripts/setup_first_he_host.sh
@@ -72,7 +72,6 @@ dnf_update() {
     name:  "*"
     state: latest
     enablerepo: "rhel*-baseos,rhel*-appstream"
-    nobest: yes
     exclude:
       - ovirt-release-master
       - ovirt-release-master-tested
--- ost_utils/pytest/fixtures/deployment.py
+++ ost_utils/pytest/fixtures/deployment.py
@@ -137,9 +137,8 @@ def deploy(
     if custom_repos is not None:
         repo_urls = package_mgmt.expand_repos(custom_repos, working_dir, ost_images_distro)
         package_mgmt.add_custom_repos(ansible_vms_to_deploy, repo_urls)
-        # TODO remove --nobest when the engine works with ansible-core
         ansible_vms_to_deploy.shell(
-            'dnf upgrade --nogpgcheck -y --disableplugin versionlock -x ovirt-release-master,ovirt-release-master-tested,ovirt-engine-appliance,rhvm-appliance,ovirt-node-ng-image-update,redhat-virtualization-host-image-update --nobest'
+            'dnf upgrade --nogpgcheck -y --disableplugin versionlock -x ovirt-release-master,ovirt-release-master-tested,ovirt-engine-appliance,rhvm-appliance,ovirt-node-ng-image-update,redhat-virtualization-host-image-update'
         )
         # check if packages from custom repos were used
         if not request.config.getoption('--skip-custom-repos-check'):

c702ed6e2ddb24bc42a2868e9175a78768180931
michalskrivanek <michal.skrivanek@redhat.com>
2022-04-07 17:16:13 +0200

    drop fapolicyd workround
    and let's see how long it keeps working....

--- common/deploy-scripts/setup_engine.sh
+++ common/deploy-scripts/setup_engine.sh
@@ -33,10 +33,3 @@ cp /usr/share/doc/ovirt-engine/mibs/* /usr/share/snmp/mibs
 
 systemctl start snmptrapd
 systemctl enable snmptrapd
-
-# FIXME - workaround for fapolicyd
-if [[ -e /usr/lib/systemd/system/fapolicyd.service ]]; then
-  sed -i '9d; 11c\ExecStart=true' /usr/lib/systemd/system/fapolicyd.service;
-  systemctl daemon-reload
-  systemctl restart fapolicyd || :
-fi

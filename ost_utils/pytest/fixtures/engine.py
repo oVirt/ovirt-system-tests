16572959b6d0c488aeec226858741d4248cd24a2
Artur Socha <asocha@redhat.com>
2022-04-22 09:45:36 +0200

    Revert "engine: Disable usage of keycloak (#120)"
    This reverts commit eee65845afd3fd9af2933067c0d4076f1e3f438a.

--- ost_utils/pytest/fixtures/engine.py
+++ ost_utils/pytest/fixtures/engine.py
@@ -60,8 +60,9 @@ def engine_webadmin_url(engine_fqdn):
 
 @pytest.fixture(scope="session")
 def keycloak_enabled(ost_images_distro, suite):
-    # turn back on when engine starts using keycloak
-    return False
+    # internally bundled Keycloak authentication is by default (via engine-setup) enabled only for upstream (el8stream)
+    # downstream (rhel) still depends on legacy AAA. Keycloak authentication can still be enabled manually
+    return ost_images_distro != 'rhel8' and suite != 'he-basic-suite-master'
 
 
 @pytest.fixture(scope="session")

db8ae07697993f9699b57dfdd8239c12ee014b13
michalskrivanek <michal.skrivanek@redhat.com>
2022-05-03 13:40:37 +0200

    fix engine API URL
    for API secure connection we need to use FQDN, the engine's certificate is only for its FQDN, not IP

--- tr-suite-master/test-scenarios/test_003_go_ovirt_client.py
+++ tr-suite-master/test-scenarios/test_003_go_ovirt_client.py
@@ -11,10 +11,10 @@ from ost_utils.pytest.fixtures.ansible import *
 from ost_utils.pytest.fixtures.engine import *
 from ost_utils.pytest.fixtures.env import suite_dir
 
-def test_run_go_ovirt_client_tests(ansible_engine, engine_api_url,
+def test_run_go_ovirt_client_tests(ansible_engine, engine_fqdn,
                            engine_full_username, engine_password):
 
     ansible_engine.shell(
-        f'OVIRT_CA_FILE=/etc/pki/ovirt-engine/ca.pem OVIRT_URL={engine_api_url} OVIRT_USERNAME={engine_full_username} OVIRT_PASSWORD={engine_password} go-ovirt-client-tests-exe -test.v'
+        f'OVIRT_CA_FILE=/etc/pki/ovirt-engine/ca.pem OVIRT_URL=https://{engine_fqdn}/ovirt-engine/api OVIRT_USERNAME={engine_full_username} OVIRT_PASSWORD={engine_password} go-ovirt-client-tests-exe -test.v'
     )
 

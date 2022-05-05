6a9209a9bb67dc2f6072fdf66a1710fdb4929bbe
Artur Socha <asocha@redhat.com>
2022-05-05 14:04:54 +0200

    Allow testing keycloak for HE suite

--- ost_utils/pytest/fixtures/engine.py
+++ ost_utils/pytest/fixtures/engine.py
@@ -62,7 +62,7 @@ def engine_webadmin_url(engine_fqdn):
 def keycloak_enabled(ost_images_distro, suite):
     # internally bundled Keycloak authentication is by default (via engine-setup) enabled only for upstream (el8stream)
     # downstream (rhel) still depends on legacy AAA. Keycloak authentication can still be enabled manually
-    return ost_images_distro != 'rhel8' and suite != 'he-basic-suite-master'
+    return ost_images_distro != 'rhel8'
 
 
 @pytest.fixture(scope="session")

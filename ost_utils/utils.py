989959736ec9192980873659a378b3ef0a228ef4
Marcin Sobczyk <msobczyk@redhat.com>
2022-06-13 10:41:27 +0200

    utils: Move 'EggTimer' class to 'ost_utils.utils'
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>
    Change-Id: I3634ae57959f1b6080435274d79991b369fe5b9c

--- ost_utils/utils.py
+++ ost_utils/utils.py
@@ -11,8 +11,24 @@ import os
 import queue
 import sys
 import threading
+import time
 
 LOGGER = logging.getLogger(__name__)
+
+
+class EggTimer:
+    def __init__(self, timeout):
+        self.timeout = timeout
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, *_):
+        pass
+
+    def elapsed(self):
+        return (time.time() - self.start_time) > self.timeout
 
 
 def _ret_via_queue(func, queue):

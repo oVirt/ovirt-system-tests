cec568c858aac14b83c7280d05a622fb0667cb43
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-06-16 12:09:53 +0200

    exclude ovirt-release-host-node from updates
    This also installs repo file on ovirt-node hosts and causes issues
    whenever those repos are not reachable. We want to run in a pre-defined
    environment only with just the --custom-repo and nothing else

--- ost_utils/pytest/fixtures/deployment.py
+++ ost_utils/pytest/fixtures/deployment.py
@@ -138,9 +138,8 @@ def deploy(
     if custom_repos is not None:
         repo_urls = package_mgmt.expand_repos(custom_repos, working_dir, ost_images_distro)
         package_mgmt.add_custom_repos(ansible_all, repo_urls)
-        # TODO workaround for https://bugzilla.redhat.com/show_bug.cgi?id=2077794
         ansible_all.shell(
-            'dnf upgrade --nogpgcheck -y --disableplugin versionlock -x ovirt-release-master,ovirt-release-master-tested,ovirt-engine-appliance,rhvm-appliance,ovirt-node-ng-image-update,redhat-virtualization-host-image-update'
+            'dnf upgrade --nogpgcheck -y --disableplugin versionlock -x ovirt-release-master,ovirt-release-master-tested,ovirt-engine-appliance,rhvm-appliance,ovirt-node-ng-image-update,redhat-virtualization-host-image-update,ovirt-release-host-node'
         )
         # check if packages from custom repos were used
         if not request.config.getoption('--skip-custom-repos-check') and not deploy_hosted_engine:

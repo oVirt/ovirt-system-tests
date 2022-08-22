735efe1e791a05988bc2971095b15a4a307e173d
Lucia Jelinkova <ljelinko@redhat.com>
2022-08-22 14:39:27 +0200

    UI tests: enhance condition for opening of the cluster dialog

--- ost_utils/selenium/page_objects/ClusterDialog.py
+++ ost_utils/selenium/page_objects/ClusterDialog.py
@@ -10,6 +10,14 @@ class ClusterDialog(EntityDialog):
     def __init__(self, ovirt_driver, action):
         super(ClusterDialog, self).__init__(ovirt_driver, 'Cluster', action)
 
+    def is_displayed(self):
+        cpu_loaded = self.ovirt_driver.retry_if_known_issue(self._is_cpu_loaded)
+        return super().is_displayed() and cpu_loaded
+
+    def _is_cpu_loaded(self):
+        cpu_element = self.ovirt_driver.find_element(By.ID, 'ClusterPopupView_cpuEditor')
+        return cpu_element.text.strip()
+
     def setDescription(self, description):
         description_field = self.ovirt_driver.find_element(By.ID, 'ClusterPopupView_descriptionEditor')
         description_field.clear()

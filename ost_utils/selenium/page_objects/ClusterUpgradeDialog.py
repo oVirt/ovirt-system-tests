ef70f76eb0414633df826c61c2614d87aff0297e
Scott J Dickerson <sdickers@redhat.com>
2022-11-14 21:39:00 -0500

    Add ui-extensions handling to driver `find_element` and `find_elements`
    Move ui-extensions shadow dom handling to the driver's `find_element`
    and `find_elements` functions.  This will allow the dialog test code
    to look more like standard GWT dialog test code.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- ost_utils/selenium/page_objects/ClusterUpgradeDialog.py
+++ ost_utils/selenium/page_objects/ClusterUpgradeDialog.py
@@ -17,62 +17,70 @@ class ClusterUpgradeDialog(Displayable):
         super(ClusterUpgradeDialog, self).__init__(ovirt_driver)
 
     def is_displayed(self):
-        dialog = self.ovirt_driver.find_dialog_root('cluster-upgrade-modal', True)
-        modal_text = dialog.find_element(By.ID, 'cluster-upgrade-modal').text
+        modal_text = self.ovirt_driver.find_element(
+            ui_extension_modal_id='cluster-upgrade-modal',
+            by=By.ID,
+            value='cluster-upgrade-modal',
+        ).text
         return 'Loading Cluster Data' not in modal_text
 
     def get_displayable_name(self):
         return 'Upgrade cluster'
 
     def toggle_check_all_hosts(self):
-        check_all = self.ovirt_driver.find_dialog_element(
-            'cluster-upgrade-modal',
+        check_all = self.ovirt_driver.find_element(
+            ui_extension_modal_id='cluster-upgrade-modal',
+            by=By.CSS_SELECTOR,
+            value='input[name="check-all"]',
             message='Select all hosts is not clickable',
-            locator=(By.CSS_SELECTOR, 'input[name="check-all"]'),
-            waitCondition=lambda el: el.is_displayed() and el.is_enabled(),
         )
         check_all.click()
 
     def toggle_check_for_upgrade(self):
-        check_upgrade = self.ovirt_driver.find_dialog_element(
-            'cluster-upgrade-modal',
+        check_upgrade = self.ovirt_driver.find_element(
+            ui_extension_modal_id='cluster-upgrade-modal',
+            by=By.ID,
+            value='upgrade-options-check-upgrade',
             message='Check for upgrade is not clickable',
-            locator=(By.ID, 'upgrade-options-check-upgrade'),
-            waitCondition=lambda el: el.is_displayed() and el.is_enabled(),
         )
         check_upgrade.click()
 
     def toggle_reboot_hosts(self):
-        reboot_hosts = self.ovirt_driver.find_dialog_element(
-            'cluster-upgrade-modal',
+        reboot_hosts = self.ovirt_driver.find_element(
+            ui_extension_modal_id='cluster-upgrade-modal',
+            by=By.ID,
+            value='upgrade-options-reboot-after',
             message='Reboot hosts is not clickable',
-            locator=(By.ID, 'upgrade-options-reboot-after'),
-            waitCondition=lambda el: el.is_displayed() and el.is_enabled(),
         )
         reboot_hosts.click()
 
     def next(self):
-        dialog = self.ovirt_driver.find_dialog_root('cluster-upgrade-modal')
-        next_button = next(
-            (b for b in dialog.find_elements(By.CSS_SELECTOR, 'footer>button.pf-m-primary') if "Next" in b.text), None
+        modal_buttons = self.ovirt_driver.find_elements(
+            ui_extension_modal_id='cluster-upgrade-modal',
+            by=By.CSS_SELECTOR,
+            value='footer>button.pf-m-primary',
         )
+        next_button = next((b for b in modal_buttons if "Next" in b.text), None)
         assert next_button is not None
         next_button.click()
 
     def upgrade(self):
-        dialog = self.ovirt_driver.find_dialog_root('cluster-upgrade-modal')
-        upgrade_button = next(
-            (b for b in dialog.find_elements(By.CSS_SELECTOR, 'footer>button.pf-m-primary') if "Upgrade" in b.text),
-            None,
+        modal_buttons = self.ovirt_driver.find_elements(
+            ui_extension_modal_id='cluster-upgrade-modal',
+            by=By.CSS_SELECTOR,
+            value='footer>button.pf-m-primary',
         )
+        upgrade_button = next((b for b in modal_buttons if "Upgrade" in b.text), None)
         assert upgrade_button is not None
         upgrade_button.click()
 
     def go_to_event_log(self):
-        dialog = self.ovirt_driver.find_dialog_root('cluster-upgrade-modal')
-        goto_button = next(
-            (b for b in dialog.find_elements(By.CSS_SELECTOR, 'button') if "Go to Event Log" in b.text), None
+        modal_buttons = self.ovirt_driver.find_elements(
+            ui_extension_modal_id='cluster-upgrade-modal',
+            by=By.CSS_SELECTOR,
+            value='button',
         )
+        goto_button = next((b for b in modal_buttons if "Go to Event Log" in b.text), None)
         assert goto_button is not None
         goto_button.click()
 

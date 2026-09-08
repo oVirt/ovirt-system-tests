e420f6a669e275d76dcf1b0616ddc9e2db1ed678
Jean-Louis Dupond <jean-louis@dupond.be>
2026-09-08 10:26:28 +0200

    Remove obsolete __future__ imports
    Remove the obsolete __future__ imports which are obsolete in Python 3
    (which is the default now).
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- basic-suite-master/test-scenarios/test_003_basic_networking.py
+++ basic-suite-master/test-scenarios/test_003_basic_networking.py
@@ -4,8 +4,6 @@
 #
 # -*- coding: utf-8 -*-
 #
-from __future__ import absolute_import
-
 import ipaddress
 import random
 
--- basic-suite-master/test-scenarios/test_007_sd_reattach.py
+++ basic-suite-master/test-scenarios/test_007_sd_reattach.py
@@ -3,8 +3,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
 #
-from __future__ import absolute_import
-
 import ovirtsdk4
 
 from ost_utils import assert_utils

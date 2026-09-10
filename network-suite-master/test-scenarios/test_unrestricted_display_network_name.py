551aa5a45219fe80ec7acd51159b1ec3998f4f5e
Jean-Louis Dupond <jean-louis@dupond.be>
2026-09-10 15:59:26 +0200

    linting: remove obsolete utf-8 headers
    Remove obsolete utf-8 headers from all files:
    -*- coding: utf-8 -*-
    
    This is not needed anymore as UTF-8 is now the default.
    https://peps.python.org/pep-3120/
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- network-suite-master/test-scenarios/test_unrestricted_display_network_name.py
+++ network-suite-master/test-scenarios/test_unrestricted_display_network_name.py
@@ -2,7 +2,6 @@
 # Copyright oVirt Authors
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
-# -*- coding: utf-8 -*-
 import pytest
 from fixtures.host import ENP2S0
 from ovirtlib import clusterlib, joblib, netattachlib, netlib, templatelib, virtlib
--- ost_utils/engine_object_names.py
+++ ost_utils/engine_object_names.py
@@ -2,8 +2,6 @@
 # Copyright oVirt Authors
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
-# -*- coding: utf-8 -*-
-#
 
 TEST_DC_NAME = 'test-dc'
 TEST_CLUSTER_NAME = 'test-cluster'
